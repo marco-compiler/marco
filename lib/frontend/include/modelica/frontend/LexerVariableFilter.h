3f93784f5fca66e7737ffc5c221991eccc60214d
Alessandro Lisi <alessandro.lisi@mail.polimi.it>
2021-04-18 15:36:58 +0200

    first commit, code complies.

---
+++ lib/frontend/include/modelica/frontend/LexerVariableFilter.h
@@ -0,0 +1,29 @@
+//
+// Created by famiglia on 15/04/21.
+//
+
+#ifndef MODELICA_LEXERVARIABLEFILTER_H
+#define MODELICA_LEXERVARIABLEFILTER_H
+
+// #include "llvm/ADT/StringRef.h"
+
+using namespace std;
+using namespace llvm;
+
+class LexerVariableFilter {
+	const string INPUT_STRING = "var1,x,y,z";
+/*
+	StringRef inputStringReference;
+	SmallVectorImpl<StringRef> elements;
+
+	public:
+	LexerVariableFilter(const string &inputString): INPUT_STRING(inputString), inputStringReference(inputString) {
+		split();
+	}
+
+	private:
+	void split(void) {
+		inputStringReference.split(elements, ',', -1, false);
+	} */
+};
+#endif	// MODELICA_LEXERVARIABLEFILTER_H
