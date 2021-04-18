164bcd5ec6484db9c8a7a45cf08cdf355ed4d622
Alessandro Lisi <alessandro.lisi@mail.polimi.it>
2021-04-18 15:39:35 +0200

    started implementation of lexer / AST

--- lib/frontend/include/modelica/frontend/LexerVariableFilter.h
+++ lib/frontend/include/modelica/frontend/LexerVariableFilter.h
@@ -5,25 +5,178 @@
 #ifndef MODELICA_LEXERVARIABLEFILTER_H
 #define MODELICA_LEXERVARIABLEFILTER_H
 
-// #include "llvm/ADT/StringRef.h"
+#include <iostream>
+#include <regex>
+
+#include "llvm/ADT/StringRef.h"
 
 using namespace std;
 using namespace llvm;
 
-class LexerVariableFilter {
-	const string INPUT_STRING = "var1,x,y,z";
-/*
-	StringRef inputStringReference;
-	SmallVectorImpl<StringRef> elements;
+namespace modelica
+{
+	//	LEXER 	//
 
-	public:
-	LexerVariableFilter(const string &inputString): INPUT_STRING(inputString), inputStringReference(inputString) {
-		split();
+	// The lexer returns tokens [0-255] if it is an unknown character, otherwise
+	// one
+	// of these for known things.
+	enum Token
+	{
+		tok_eof = -1,
+
+		// commands
+		tok_der = -2,
+
+		// primary
+		tok_identifier = -4,
+
+	};
+
+	static std::string IdentifierStr;	 // Filled in if tok_identifier
+	static double NumVal;							 // Filled in if tok_number
+
+	static bool isValidIDChar(char c)
+	{
+		return (isalnum(c) || (c == '(') || (c == ')') || (c == '_'));
+	}
+	static const char *inputString;
+	static unsigned short i;
+	/// gettok - Return the next token from standard input.
+	static int gettok()
+	{
+		static int LastChar = ' ';	// current character
+
+		// Skip any whitespace between tokens.
+		while (isspace(LastChar))
+			LastChar = inputString[i];
+		i++;
+
+		// VARIABLES AND DERIVATIVES
+		if (isalpha(LastChar) || (LastChar == '_'))
+		{	 // identifier: [a-zA-Z][a-zA-Z0-9]* or underscore
+			IdentifierStr = LastChar;
+			while (isValidIDChar(
+					(LastChar = getchar())))	// keep reading until identifier stops
+				IdentifierStr += LastChar;
+
+			if (IdentifierStr == "der(")
+				return tok_der;
+
+			return tok_identifier;
+		}
+		return LastChar;
 	}
 
-	private:
-	void split(void) {
-		inputStringReference.split(elements, ',', -1, false);
-	} */
-};
+	// === AST / PARSER === //
+
+	/// ExprAST - Base class for all expression nodes.
+	class ExprAST
+	{
+		public:
+		virtual ~ExprAST() {}
+	};
+
+	/// NumberExprAST - Expression class for numeric literals like "1.0".
+	class VariableExprAST: public ExprAST
+	{
+		string identifier;
+
+		public:
+		VariableExprAST(const string &identifier): identifier(identifier) {}
+	};
+
+	class DerivativeExprAST: public ExprAST
+	{
+		VariableExprAST derivedVariable;
+
+		public:
+		DerivativeExprAST(const VariableExprAST &derivedVariable)
+				: derivedVariable(derivedVariable)
+		{
+		}
+	};
+
+	class RegexExprAST: public ExprAST
+	{
+		basic_regex<string> regex;
+
+		public:
+		RegexExprAST(const basic_regex<string> &regex): regex(regex) {}
+	};
+
+	/// CurTok/getNextToken - Provide a simple token buffer.  CurTok is the
+	/// current
+	/// token the parser is looking at.  getNextToken reads another token from
+	/// the lexer and updates CurTok with its results.
+	static int CurTok;
+	static int getNextToken() { return CurTok = gettok(); }
+
+	/// var ::= varID
+	static std::unique_ptr<ExprAST> ParseVariableExpr()
+	{
+		std::string IdName = IdentifierStr;
+		auto Result = std::make_unique<VariableExprAST>(IdName);
+		getNextToken();	 // consume the number
+		return std::move(Result);
+	}
+
+	static std::unique_ptr<ExprAST> ParseDerivative()
+	{
+		getNextToken();	 // eat "der("
+
+		// create derivative
+		std::string IdName = IdentifierStr;
+		auto Result = std::make_unique<DerivativeExprAST>(IdName);
+		getNextToken();	 // consume the identifier
+		getNextToken();	 // eat ")"
+		return std::move(Result);
+		getNextToken();	 // eat ")"
+	}
+	
+	class LexerVariableFilter
+	{
+		StringRef inputStringReference;
+
+
+
+		// === CLASS IMPL ===== //
+
+		public:
+		LexerVariableFilter(const string &inputString)
+				: inputStringReference(inputString)
+		{
+		}
+
+		void split(void)
+		{
+			SmallVector<StringRef> elements;
+			cout << "Splitting: " << inputStringReference.str() << endl;
+			inputStringReference.split(elements, ',', -1, false);
+			for (const auto &item : elements)
+			{
+				string ele = item.str();
+				cout << ele << endl;
+
+				while (true)
+				{
+					cout << "ready:";
+					switch (CurTok)
+					{
+						case tok_eof:
+							return;
+						case tok_der:
+							ParseDerivative();
+							break;
+						case tok_identifier:
+							ParseVariableExpr();
+							break;
+						default:
+							getNextToken();
+							break;
+					}
+				}
+			}
+		};
+	};
+}	 // namespace modelica
 #endif	// MODELICA_LEXERVARIABLEFILTER_H
--- tool/OmcC/src/Main.cpp
+++ tool/OmcC/src/Main.cpp
@@ -2,20 +2,20 @@
 #include "llvm/Support/Error.h"
 #include "llvm/Support/FileSystem.h"
 #include "llvm/Support/MemoryBuffer.h"
-#include "marco/frontend/Parser.h"
-#include "marco/frontend/Passes.h"
-#include "marco/frontend/SymbolTable.hpp"
-#include "marco/frontend/passes/TypeCheckingPass.h"
-#include "marco/lowerer/Lowerer.hpp"
-#include "marco/matching/Matching.hpp"
-#include "marco/matching/SccCollapsing.hpp"
-#include "marco/matching/Schedule.hpp"
-#include "marco/model/AssignModel.hpp"
-#include "marco/model/ModVariable.hpp"
-#include "marco/omcToModel/OmcToModelPass.hpp"
-#include "marco/passes/ConstantFold.hpp"
-#include "marco/passes/ForwardEuler.hpp"
-#include "marco/passes/SolveModel.hpp"
+#include "modelica/frontend/Parser.hpp"
+#include "modelica/frontend/Passes.h"
+#include "modelica/frontend/SymbolTable.hpp"
+#include "modelica/frontend/passes/TypeCheckingPass.h"
+#include "modelica/lowerer/Lowerer.hpp"
+#include "modelica/matching/Matching.hpp"
+#include "modelica/matching/SccCollapsing.hpp"
+#include "modelica/matching/Schedule.hpp"
+#include "modelica/model/AssignModel.hpp"
+#include "modelica/model/ModVariable.hpp"
+#include "modelica/omcToModel/OmcToModelPass.hpp"
+#include "modelica/passes/ConstantFold.hpp"
+#include "modelica/passes/SolveModel.hpp"
+#include "modelica/frontend/LexerVariableFilter.h"
 
 using namespace marco;
 using namespace llvm;
@@ -138,6 +138,11 @@ int main(int argc, char* argv[])
 		errs() << error.message();
 	}
 
+	//======= TEMPORARY CODE ====/
+	std:string test = "varX,y,Z123,_tau";
+	LexerVariableFilter lvf(test);
+	lvf.split();
+	//===========================
 	auto buffer = exitOnErr(errorOrToExpected(move(errorOrBuffer)));
 	frontend::Parser parser(buffer->getBufferStart());
 	auto ast = exitOnErr(parser.classDefinition());
