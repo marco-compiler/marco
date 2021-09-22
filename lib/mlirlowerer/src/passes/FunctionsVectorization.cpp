82b3e6591a2150d60beb58689536f06e622c0daf
Michele Scuttari <mscuttari@users.noreply.github.com>
2021-09-22 15:11:24 +0200

    Code improvements

--- lib/mlirlowerer/src/passes/FunctionsVectorization.cpp
+++ lib/mlirlowerer/src/passes/FunctionsVectorization.cpp
@@ -8,7 +8,15 @@ using namespace modelica;
 
 static unsigned int getVectorizationRank(VectorizableOpInterface op)
 {
-	llvm::SmallVector<long, 2> expectedRanks;
+	// Only functions with exactly one result can be vectorized, as per
+	// Modelica language specification.
+
+	if (op->getNumResults() != 1)
+		return 0;
+
+	// List of the additional dimensions with respect to the expected scalar
+	// arguments.
+
 	llvm::SmallVector<long, 3> dimensions;
 
 	auto args = op.getArgs();
@@ -75,11 +83,6 @@ static unsigned int getVectorizationRank(VectorizableOpInterface op)
 	return dimensions.size();
 }
 
-static bool isVectorized(VectorizableOpInterface op)
-{
-	return getVectorizationRank(op) != 0;
-}
-
 static mlir::Value allocate(mlir::OpBuilder& builder, mlir::Location loc, ArrayType arrayType, mlir::ValueRange dynamicDimensions = llvm::None)
 {
 	if (arrayType.getAllocationScope() == BufferAllocationScope::unknown)
@@ -89,96 +92,6 @@ static mlir::Value allocate(mlir::OpBuilder& builder, mlir::Location loc, ArrayT
 		return builder.create<AllocaOp>(loc, arrayType.getElementType(), arrayType.getShape(), dynamicDimensions);
 
 	return builder.create<AllocOp>(loc, arrayType.getElementType(), arrayType.getShape(), dynamicDimensions, true);
-}
-
-static void scalarize(mlir::OpBuilder& builder, VectorizableOpInterface op, FunctionsVectorizationOptions options)
-{
-	mlir::Location loc = op->getLoc();
-	mlir::OpBuilder::InsertionGuard guard(builder);
-	builder.setInsertionPoint(op);
-
-	unsigned int vectorizationRank = getVectorizationRank(op);
-	mlir::ValueRange args = op.getArgs();
-
-	// Allocate the result arrays
-	llvm::SmallVector<mlir::Value, 3> results;
-
-	for (const auto& resultType : op->getResultTypes())
-	{
-		assert(resultType.isa<ArrayType>());
-		llvm::SmallVector<long, 3> shape;
-		llvm::SmallVector<mlir::Value, 3> dynamicDimensions;
-
-		for (const auto& dimension : llvm::enumerate(resultType.cast<ArrayType>().getShape()))
-		{
-			shape.push_back(dimension.value());
-
-			if (dimension.value() == -1)
-			{
-				// Get the actual size from the first operand. Others should have
-				// the same size by construction.
-
-				mlir::Value index = builder.create<mlir::ConstantOp>(loc, builder.getIndexAttr(dimension.index()));
-				dynamicDimensions.push_back(builder.create<DimOp>(loc, args[0], index));
-			}
-		}
-
-		auto arrayType = ArrayType::get(
-				resultType.getContext(),
-				resultType.cast<ArrayType>().getAllocationScope(),
-				resultType.cast<ArrayType>().getElementType(),
-				shape);
-
-		results.push_back(allocate(builder, loc, arrayType, dynamicDimensions));
-	}
-
-	if (options.assertions)
-	{
-		llvm::SmallVector<mlir::Value, 3> indexes;
-
-		for (size_t i = 0; i < vectorizationRank; ++i)
-			builder.create<mlir::ConstantOp>(loc, builder.getIndexAttr(i));
-
-		for (mlir::Value index : indexes)
-		{
-			llvm::SmallVector<mlir::Value, 3> dimensions;
-
-			for (mlir::Value arg : args)
-				dimensions.push_back(builder.create<DimOp>(loc, arg, index));
-
-			for (size_t i = 1; i < dimensions.size(); ++i)
-			{
-				mlir::Value condition = builder.create<mlir::CmpIOp>(loc, mlir::CmpIPredicate::eq, dimensions[0], dimensions[i]);
-				builder.create<mlir::AssertOp>(loc, condition, "Incompatible dimensions for vectorized function arguments");
-			}
-		}
-	}
-
-	mlir::Value zero = builder.create<mlir::ConstantOp>(op->getLoc(), builder.getIndexAttr(0));
-	mlir::Value one = builder.create<mlir::ConstantOp>(op->getLoc(), builder.getIndexAttr(1));
-	llvm::SmallVector<mlir::Value, 3> indexes;
-
-	for (unsigned int i = 0; i < vectorizationRank; ++i)
-	{
-		mlir::Value index = builder.create<ConstantOp>(op->getLoc(), builder.getIndexAttr(i));
-		mlir::Value dimension = builder.create<DimOp>(op->getLoc(), op.getArgs()[0], index);
-		auto forOp = builder.create<mlir::scf::ForOp>(op->getLoc(), zero, dimension, one);
-		indexes.push_back(forOp.getInductionVar());
-		builder.setInsertionPointToStart(forOp.getBody());
-	}
-
-	mlir::ValueRange scalarizedResults = op.scalarize(builder, indexes);
-
-	// Copy the (not necessarily) scalar results into the result arrays
-	for (auto result : llvm::enumerate(scalarizedResults))
-	{
-		mlir::Value subscript = builder.create<SubscriptionOp>(loc, results[result.index()], indexes);
-		builder.create<AssignmentOp>(loc, result.value(), subscript);
-	}
-
-	// Replace the original operation with the newly allocated arrays
-	op->replaceAllUsesWith(results);
-	op->erase();
 }
 
 class FunctionsVectorizationPass: public mlir::PassWrapper<FunctionsVectorizationPass, mlir::OperationPass<mlir::ModuleOp>>
@@ -201,13 +114,119 @@ class FunctionsVectorizationPass: public mlir::PassWrapper<FunctionsVectorizatio
 		auto module = getOperation();
 		mlir::OpBuilder builder(module);
 
-		module->walk([&](VectorizableOpInterface op) {
-			if (isVectorized(op))
-				scalarize(builder, op, options);
+		llvm::SmallVector<std::pair<VectorizableOpInterface, unsigned int>, 3> vectorizedOps;
+
+		module->walk([&vectorizedOps](VectorizableOpInterface op) {
+			if (unsigned int rank = getVectorizationRank(op); rank != 0)
+				vectorizedOps.emplace_back(op, rank);
 		});
+
+		for (auto& op : vectorizedOps)
+			if (auto result = scalarize(builder, op.first, op.second); failed(result))
+				return signalPassFailure();
 	}
 
 	private:
+	mlir::LogicalResult scalarize(mlir::OpBuilder& builder, VectorizableOpInterface op, unsigned int vectorizationRank)
+	{
+		mlir::Location loc = op->getLoc();
+		mlir::OpBuilder::InsertionGuard guard(builder);
+		builder.setInsertionPoint(op);
+
+		mlir::ValueRange args = op.getArgs();
+
+		// Allocate the result arrays. In theory, vectorizable operation has one
+		// and only one result as per the Modelica language specification.
+		// However, in order to be resilient to specification changes, the
+		// scalarization process has been implemented also to consider multiple
+		// results. It must be noted that this extension has no performance
+		// drawbacks, as it will just stop after processing the first and only
+		// expected result.
+
+		llvm::SmallVector<mlir::Value, 3> results;
+
+		for (const auto& resultType : op->getResultTypes())
+		{
+			assert(resultType.isa<ArrayType>());
+			llvm::SmallVector<long, 3> shape;
+			llvm::SmallVector<mlir::Value, 3> dynamicDimensions;
+
+			for (const auto& dimension : llvm::enumerate(resultType.cast<ArrayType>().getShape()))
+			{
+				shape.push_back(dimension.value());
+
+				if (dimension.value() == -1)
+				{
+					// Get the actual size from the first operand. Others should have
+					// the same size by construction.
+
+					mlir::Value index = builder.create<mlir::ConstantOp>(loc, builder.getIndexAttr(dimension.index()));
+					dynamicDimensions.push_back(builder.create<DimOp>(loc, args[0], index));
+				}
+			}
+
+			auto arrayType = ArrayType::get(
+					resultType.getContext(),
+					resultType.cast<ArrayType>().getAllocationScope(),
+					resultType.cast<ArrayType>().getElementType(),
+					shape);
+
+			results.push_back(allocate(builder, loc, arrayType, dynamicDimensions));
+		}
+
+		// If runtime assertions are enabled, check if the dimensions match
+
+		if (options.assertions)
+		{
+			llvm::SmallVector<mlir::Value, 3> indexes;
+
+			for (size_t i = 0; i < vectorizationRank; ++i)
+				builder.create<mlir::ConstantOp>(loc, builder.getIndexAttr(i));
+
+			for (mlir::Value index : indexes)
+			{
+				llvm::SmallVector<mlir::Value, 3> dimensions;
+
+				for (mlir::Value arg : args)
+					dimensions.push_back(builder.create<DimOp>(loc, arg, index));
+
+				for (size_t i = 1; i < dimensions.size(); ++i)
+				{
+					mlir::Value condition = builder.create<mlir::CmpIOp>(loc, mlir::CmpIPredicate::eq, dimensions[0], dimensions[i]);
+					builder.create<mlir::AssertOp>(loc, condition, "Incompatible dimensions for vectorized function arguments");
+				}
+			}
+		}
+
+		mlir::Value zero = builder.create<mlir::ConstantOp>(op->getLoc(), builder.getIndexAttr(0));
+		mlir::Value one = builder.create<mlir::ConstantOp>(op->getLoc(), builder.getIndexAttr(1));
+		llvm::SmallVector<mlir::Value, 3> indexes;
+
+		for (unsigned int i = 0; i < vectorizationRank; ++i)
+		{
+			mlir::Value index = builder.create<ConstantOp>(op->getLoc(), builder.getIndexAttr(i));
+			mlir::Value dimension = builder.create<DimOp>(op->getLoc(), op.getArgs()[0], index);
+			auto forOp = builder.create<mlir::scf::ForOp>(op->getLoc(), zero, dimension, one);
+			indexes.push_back(forOp.getInductionVar());
+			builder.setInsertionPointToStart(forOp.getBody());
+		}
+
+		mlir::ValueRange scalarizedResults = op.scalarize(builder, indexes);
+
+		// Copy the (not necessarily) scalar results into the result arrays
+		for (auto result : llvm::enumerate(scalarizedResults))
+		{
+			mlir::Value subscript = builder.create<SubscriptionOp>(loc, results[result.index()], indexes);
+			builder.create<AssignmentOp>(loc, result.value(), subscript);
+		}
+
+		// Replace the original operation with the newly allocated arrays
+		op->replaceAllUsesWith(results);
+		op->erase();
+
+		return mlir::success();
+	}
+
 	FunctionsVectorizationOptions options;
 };
 
