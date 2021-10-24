ff26c06c564e036803bd13452cf6c935cdeafe38
Camillucci <nicola.camillucci@mail.polimi.it>
2021-10-24 12:50:19 +0200

    Update replace uses method

--- test/gtest/matching/LinSolverTest.cpp
+++ test/gtest/matching/LinSolverTest.cpp
@@ -1,4 +1,5 @@
 #include "gtest/gtest.h"
+#include <marco/mlirlowerer/dialects/modelica/ModelicaDialect.h>
 #include <marco/mlirlowerer/passes/matching/LinSolver.h>
 #include <marco/mlirlowerer/passes/matching/Matching.h>
 #include <marco/mlirlowerer/passes/model/Equation.h>
@@ -7,6 +8,7 @@
 #include "../TestingUtils.h"
 
 using namespace marco::codegen::model;
+using namespace marco::codegen::modelica;
 
 TEST(LinSolverTest, LinearySolveTest)
 {
@@ -58,4 +60,41 @@ TEST(LinSolverTest, LinearySolveTest)
 
 	EXPECT_TRUE(equations[0].lhs().isReferenceAccess());
 	EXPECT_TRUE(equations[1].lhs().isReferenceAccess());
+}
+
+TEST(LinSolverTest, DifferentReplaceUses)
+{
+	std::string stringModel = "model SubstituteTwice "
+														"Real[2] x; "
+														"Real y; "
+														"equation "
+														"x[1] = 2.0; "
+														"x[2] = 1.0; "
+														"der(y) = x[1] + x[2]; "
+														"end SubstituteTwice; ";
+
+	mlir::MLIRContext context;
+	Model model;
+	makeModel(context, stringModel, model);
+	mlir::OpBuilder builder(model.getOp());
+
+	if (failed(match(model, 1000)))
+		FAIL();
+
+	EXPECT_EQ(model.getEquations().size(), 3);
+
+	Equation destination = model.getEquations()[2];
+	EXPECT_TRUE(destination.lhs().isReferenceAccess());
+	EXPECT_TRUE(destination.rhs().isOperation());
+
+	replaceUses(builder, model.getEquations()[0], destination);
+	EXPECT_TRUE(destination.rhs().isOperation());
+
+	replaceUses(builder, model.getEquations()[1], destination);
+	EXPECT_TRUE(destination.rhs().isConstant());
+	EXPECT_TRUE(mlir::isa<ConstantOp>(destination.rhs().getOp()));
+
+	auto attribute = mlir::cast<ConstantOp>(destination.rhs().getOp()).value();
+	EXPECT_TRUE(attribute.isa<RealAttribute>());
+	EXPECT_EQ(attribute.cast<RealAttribute>().getValue(), 3.0);
 }
