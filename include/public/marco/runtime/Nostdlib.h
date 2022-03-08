0e8f7a950afc0896617629a827b473e9db14e8bc
Stefano Azzone <stefano.azzone@mail.polimi.it>
2022-03-08 15:46:35 +0100

    Fill temporarily disabled: simpleDer works only without -nostdlib

--- include/public/marco/runtime/Nostdlib.h
+++ include/public/marco/runtime/Nostdlib.h
@@ -2,3 +2,4 @@ extern "C" __declspec(dllexport) void* memmove(void* dstpp, const void* srcpp, s
 extern "C" __declspec(dllexport) void* memcpy(void* dstpp, const void* srcpp, size_t len);
 extern "C" __declspec(dllexport) void* memset(void* s, int c,  size_t len);
 extern "C" __declspec(dllexport) void runtimeMemset(char *p, char c, int l);
+extern "C" __declspec(dllexport) int printf(const char* format, ...);
