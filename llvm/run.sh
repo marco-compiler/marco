1ba7c68b41358c7d274002837032e709ee5cc93c
Stefano Azzone <stefano.azzone@mail.polimi.it>
2022-04-22 10:02:22 +0200

    Added script to download, patch and build llvm PKGBUILD for mingw64 with mlir

---
+++ llvm/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Download repo of PKGBUILDS for mingw64, checkout version 13 of llvm PKGBUILD
+git clone https://github.com/msys2/MINGW-packages.git
+cd MINGW-packages
+git checkout b845c4cc40a43c60558f0df1d4f3aed6b930fcf3
+mv mingw-w64-clang/ ../build
+cd ..
+rm -rf MINGW-packages
+
+# Run llvm PKGBUILD patching it to compile mlir beforehand
+cp marco.patch build
+cd build
+patch PKGBUILD < marco.patch
+makepkg -Csi --skipinteg --noconfirm --nocheck
+cd ..
