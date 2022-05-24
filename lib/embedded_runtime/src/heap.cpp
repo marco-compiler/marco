9a438cf5a2945e18b7d124f1d1d55ec59cb610ee
GioBr <giorgiobrugali@gmail.com>
2022-05-24 09:22:17 +0200

    malloc and free fixed

--- lib/embedded_runtime/src/heap.cpp
+++ lib/embedded_runtime/src/heap.cpp
@@ -3,64 +3,6 @@
 #include <assert.h>
 
 extern SerialPort serial;
-/*
-
-extern char *_Min_Stack_Size;
-extern char _end;     
-size_t heap_last;
-
-
-size_t* sbrk(const int incr) {
- 
-  static char *heap_end;
-  char *prev_heap_end;
-  extern char _end;
-
-  if (heap_end == 0) {
-    heap_end = &_end;
-  }
-  prev_heap_end = heap_end;
-  if (heap_end + incr > _Min_Stack_Size) {
-    return nullptr;
-  }
-
-  heap_end += incr;
-  heap_last = (size_t) heap_end;
-  return (size_t*) prev_heap_end;
-}
-
-
-size_t *find_free_space(size_t size) {
-  size_t *header = (size_t*)_end;
-  while (*header < heap_last) {
-  if (!(*header & 1) && *header >= size)
-    return header;
-    header = (size_t*)((char*)header + (*header & ~1L));
-  }
-  return nullptr;
-}
-
-
-void *malloc(size_t size) {
-  size_t blk_size = ALIGN(size + HEADER_SIZE);
-  size_t *header = (size_t*) find_free_space(blk_size);
-  if (header) {
-  *header = *header | 1;
-  } else {
-  header = (size_t*) sbrk(blk_size);
-  *header = blk_size | 1;
-}
-  return (char *)header + HEADER_SIZE;
-}
-
-
-
-
-void free(void *ptr) {
-    size_t *header = (size_t*)((char *)ptr - HEADER_SIZE);
-    *header = *header & ~1L; // unmark allocated bit
-    ptr=nullptr;
-}*/
 
 
 static size_t *__sbrk_heap_end = NULL;
@@ -98,16 +40,19 @@ typedef struct Block{
 
 #define HEADER_SIZE sizeof(block)
 
-bool first = 1;
 block* head_block = NULL;
 block* last_allocated = NULL;
 
 block *find_free_space(size_t size){
   block* it;
   it = head_block;
-  //while(it->next != nullptr){
-  while (it && !(it->free && it->size >= size)){
+  while(it != nullptr){
     serial.write("searching\n\r");
+    serial.write((int)(it->free) );
+    serial.write(" Size ");
+    serial.write((int)(it->size ));
+    serial.write(" Block size ");
+    serial.write((int)size);
     if(it->free && size <= it->size){
       serial.write("space reused\n\r");
       return it;
@@ -116,16 +61,19 @@ block *find_free_space(size_t size){
   }
   return NULL;
 }
+
 void *malloc(size_t size){
   size_t blk_size = ALIGN(size + HEADER_SIZE);
   block *header;
+
   if(size <= 0) return NULL;
-  
   header = find_free_space(blk_size);
-  header->size = blk_size;
+  
   if(header){
     header->free = 0;
-    return (void*) header;
+    header->next = NULL;
+    header->size = blk_size;
+    return (header+1);
   }else{
     header= (block* ) sbrk(0);
     if(sbrk(blk_size) == (void*)-1){
@@ -133,16 +81,18 @@ void *malloc(size_t size){
       }
     }
     header->free = 0;
-    last_allocated->next = header;
+    header->next = NULL;
+    header->size = blk_size;
+    if(!head_block) 
+      head_block = header;
+    else last_allocated->next = header;
     last_allocated = header;
-    return (void*) header;
+    return (header+1);
     
   }
 
 void free(void *ptr){
-  block *header = (block*) ptr;
-  header->free = 0;
-  //header->next = nullptr;
-  header->size = 0;
-  //ptr = nullptr;
+  block *header ;
+  header = (block*) ptr - 1;
+  header->free = 1;
 }
