50ed407fef15f91ecb286f5b00ca28a27bb08af4
Michele Scuttari <michele.scuttari@outlook.com>
2022-09-06 10:25:27 +0200

    Add SumOp to vector conversion

--- lib/Codegen/Conversion/ModelicaToVector/ModelicaToVector.cpp
+++ lib/Codegen/Conversion/ModelicaToVector/ModelicaToVector.cpp
@@ -67,11 +67,11 @@ namespace
 
 namespace
 {
-  template<typename ElementType>
+  template<typename... ElementType>
   struct AddOpArraysLowering : public ModelicaOpConversionPattern<AddOp>
   {
     public:
-      using ModelicaOpConversionPattern<AddOp>::ModelicaOpConversionPattern;
+      using ModelicaOpConversionPattern::ModelicaOpConversionPattern;
 
       mlir::LogicalResult matchAndRewrite(AddOp op, OpAdaptor adaptor, mlir::ConversionPatternRewriter& rewriter) const override
       {
@@ -80,7 +80,7 @@ namespace
         auto resultConvertedType = getTypeConverter()->convertType(op.getResult().getType());
         mlir::Type elementType = resultConvertedType.template cast<mlir::MemRefType>().getElementType();
 
-        if (!elementType.isa<ElementType>()) {
+        if (!mlir::isa<ElementType...>(elementType)) {
           return mlir::failure();
         }
 
@@ -100,18 +100,7 @@ namespace
       virtual mlir::Value createVectorOp(mlir::OpBuilder& builder, mlir::Location loc, mlir::Value lhs, mlir::Value rhs) const = 0;
   };
 
-  struct AddOpIndexArraysLowering : public AddOpArraysLowering<mlir::IndexType>
-  {
-    public:
-      using AddOpArraysLowering::AddOpArraysLowering;
-
-      mlir::Value createVectorOp(mlir::OpBuilder& builder, mlir::Location loc, mlir::Value lhs, mlir::Value rhs) const override
-      {
-        return builder.create<mlir::arith::AddIOp>(loc, lhs, rhs);
-      }
-  };
-
-  struct AddOpIntegerArraysLowering : public AddOpArraysLowering<mlir::IntegerType>
+  struct AddOpIntegerLikeArraysLowering : public AddOpArraysLowering<mlir::IndexType, mlir::IntegerType>
   {
     public:
       using AddOpArraysLowering::AddOpArraysLowering;
@@ -140,7 +129,7 @@ namespace
 
 namespace
 {
-  template<typename ElementType>
+  template<typename... ElementType>
   struct SubOpArraysLowering : public ModelicaOpConversionPattern<SubOp>
   {
     public:
@@ -153,7 +142,7 @@ namespace
         auto resultConvertedType = getTypeConverter()->convertType(op.getResult().getType());
         mlir::Type elementType = resultConvertedType.template cast<mlir::MemRefType>().getElementType();
 
-        if (!elementType.isa<ElementType>()) {
+        if (!mlir::isa<ElementType...>(elementType)) {
           return mlir::failure();
         }
 
@@ -173,18 +162,7 @@ namespace
       virtual mlir::Value createVectorOp(mlir::OpBuilder& builder, mlir::Location loc, mlir::Value lhs, mlir::Value rhs) const = 0;
   };
 
-  struct SubOpIndexArraysLowering : public SubOpArraysLowering<mlir::IndexType>
-  {
-    public:
-      using SubOpArraysLowering::SubOpArraysLowering;
-
-      mlir::Value createVectorOp(mlir::OpBuilder& builder, mlir::Location loc, mlir::Value lhs, mlir::Value rhs) const override
-      {
-        return builder.create<mlir::arith::SubIOp>(loc, lhs, rhs);
-      }
-  };
-
-  struct SubOpIntegerArraysLowering : public SubOpArraysLowering<mlir::IntegerType>
+  struct SubOpIntegerLikeArraysLowering : public SubOpArraysLowering<mlir::IndexType, mlir::IntegerType>
   {
     public:
       using SubOpArraysLowering::SubOpArraysLowering;
@@ -203,6 +181,96 @@ namespace
       mlir::Value createVectorOp(mlir::OpBuilder& builder, mlir::Location loc, mlir::Value lhs, mlir::Value rhs) const override
       {
         return builder.create<mlir::arith::SubFOp>(loc, lhs, rhs);
+      }
+  };
+}
+
+//===----------------------------------------------------------------------===//
+// SumOp
+//===----------------------------------------------------------------------===//
+
+namespace
+{
+  struct SumOpCastPattern : public ModelicaOpRewritePattern<SumOp>
+  {
+    using ModelicaOpRewritePattern<SumOp>::ModelicaOpRewritePattern;
+
+    mlir::LogicalResult match(SumOp op) const override
+    {
+      auto arrayType = op.getArray().getType().cast<ArrayType>();
+      mlir::Type resultType = op.getResult().getType();
+
+      return mlir::LogicalResult::success(arrayType.getElementType() != resultType);
+    }
+
+    void rewrite(SumOp op, mlir::PatternRewriter& rewriter) const override
+    {
+      auto loc = op.getLoc();
+      auto resultType = op.getArray().getType().cast<ArrayType>().getElementType();
+      mlir::Value result = rewriter.create<SumOp>(loc, resultType, op.getArray());
+      rewriter.replaceOpWithNewOp<CastOp>(op, op.getResult().getType(), result);
+    }
+  };
+
+  template<typename... ElementType>
+  struct SumOpLowering : public ModelicaOpConversionPattern<SumOp>
+  {
+    public:
+      using ModelicaOpConversionPattern<SumOp>::ModelicaOpConversionPattern;
+
+      mlir::LogicalResult matchAndRewrite(SumOp op, OpAdaptor adaptor, mlir::ConversionPatternRewriter& rewriter) const override
+      {
+        mlir::Type elementType = adaptor.getArray().getType().template cast<mlir::MemRefType>().getElementType();
+        mlir::Type resultType = getTypeConverter()->convertType(op.getResult().getType());
+
+        if (elementType != resultType || !mlir::isa<ElementType...>(resultType)) {
+          return mlir::failure();
+        }
+
+        auto loc = op.getLoc();
+        mlir::Value array = readVector(rewriter, loc, adaptor.getArray());
+        mlir::Value acc = createAccumulator(rewriter, loc, resultType);
+
+        int64_t rank = array.getType().template cast<mlir::VectorType>().getRank();
+
+        if (rank == 1) {
+          rewriter.template replaceOpWithNewOp<mlir::vector::ReductionOp>(
+              op, mlir::vector::CombiningKind::ADD, array, acc);
+        } else {
+          llvm::SmallVector<bool, 2> reductionMask(rank, true);
+
+          rewriter.template replaceOpWithNewOp<mlir::vector::MultiDimReductionOp>(
+              op, array, acc, reductionMask, mlir::vector::CombiningKind::ADD);
+        }
+
+        return mlir::success();
+      }
+
+    protected:
+      virtual mlir::Value createAccumulator(mlir::OpBuilder& builder, mlir::Location loc, mlir::Type type) const = 0;
+  };
+
+  struct SumOpIntegerLikeLowering : public SumOpLowering<mlir::IndexType, mlir::IntegerType>
+  {
+    public:
+      using SumOpLowering::SumOpLowering;
+
+    protected:
+      mlir::Value createAccumulator(mlir::OpBuilder& builder, mlir::Location loc, mlir::Type type) const override
+      {
+        return builder.create<mlir::arith::ConstantOp>(loc, builder.getIntegerAttr(type, 0));
+      }
+  };
+
+  struct SumOpFloatLowering : public SumOpLowering<mlir::FloatType>
+  {
+    public:
+      using SumOpLowering::SumOpLowering;
+
+    protected:
+      mlir::Value createAccumulator(mlir::OpBuilder& builder, mlir::Location loc, mlir::Type type) const override
+      {
+        return builder.create<mlir::arith::ConstantOp>(loc, builder.getFloatAttr(type, 0));
       }
   };
 }
@@ -242,14 +310,19 @@ static void populateModelicaToVectorPatterns(
     mlir::TypeConverter& typeConverter)
 {
   patterns.insert<
-      AddOpIndexArraysLowering,
-      AddOpIntegerArraysLowering,
+      AddOpIntegerLikeArraysLowering,
       AddOpFloatArraysLowering>(typeConverter, context);
 
   patterns.insert<
-      SubOpIndexArraysLowering,
-      SubOpIntegerArraysLowering,
+      SubOpIntegerLikeArraysLowering,
       SubOpFloatArraysLowering>(typeConverter, context);
+
+  patterns.insert<
+      SumOpCastPattern>(context);
+
+  patterns.insert<
+      SumOpIntegerLikeLowering,
+      SumOpFloatLowering>(typeConverter, context);
 
   patterns.insert<
       TransposeOpLowering>(typeConverter, context);
@@ -331,6 +404,8 @@ namespace
           mlir::Type resultType = op.getResult().getType().cast<ArrayType>().getElementType();
           return argType != resultType;
         });
+
+        target.addIllegalOp<SumOp>();
 
         TypeConverter typeConverter(bitWidth);
 
