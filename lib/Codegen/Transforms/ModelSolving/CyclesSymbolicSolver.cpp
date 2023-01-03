d6dcd494e4d78110d83ade905cb6cbde6ccedd6a
Stefano Azzone <stefano.azzone@mail.polimi.it>
2023-01-03 17:39:35 +0100

    Added constructor and solve method

--- lib/Codegen/Transforms/ModelSolving/CyclesSymbolicSolver.cpp
+++ lib/Codegen/Transforms/ModelSolving/CyclesSymbolicSolver.cpp
@@ -1,3 +1,15 @@
 #include "marco/Codegen/Transforms/ModelSolving/CyclesSymbolicSolver.h"
 
 using namespace marco::codegen;
+
+CyclesSymbolicSolver::CyclesSymbolicSolver(mlir::OpBuilder& builder) : builder(builder)
+{
+
+}
+
+bool CyclesSymbolicSolver::solve()
+{
+  return false;
+}
+
+
