109e80976531c8180734d8a069c958b28faf15ca
Stefano Azzone <stefano.azzone@mail.polimi.it>
2023-01-03 16:52:54 +0100

    Initialize the symbolic solver

---
+++ include/private/marco/Codegen/Transforms/ModelSolving/CyclesSymbolicSolver.h
@@ -0,0 +1,10 @@
+#ifndef MARCO_CYCLESSYMBOLICSOLVER_H
+#define MARCO_CYCLESSYMBOLICSOLVER_H
+
+namespace marco::codegen {
+  class CyclesSymbolicSolver
+  {
+  };
+}
+
+#endif//MARCO_CYCLESSYMBOLICSOLVER_H
