82195fe6feffe25a37888357ff91beef904c9ab7
Michele Scuttari <michele.scuttari@outlook.com>
2023-02-08 21:54:26 +0100

    Explicit cast insertion: reduce symbols lookup complexity

--- lib/Codegen/Transforms/ExplicitCastInsertion.cpp
+++ lib/Codegen/Transforms/ExplicitCastInsertion.cpp
@@ -14,83 +14,105 @@ using namespace ::mlir::modelica;
 
 namespace
 {
-  struct CallOpScalarPattern : public mlir::OpRewritePattern<CallOp>
+  class CallOpScalarPattern : public mlir::OpRewritePattern<CallOp>
   {
-    using mlir::OpRewritePattern<CallOp>::OpRewritePattern;
-
-    mlir::LogicalResult match(CallOp op) const override
-    {
-      auto module = op->getParentOfType<mlir::ModuleOp>();
-      auto callee = module.lookupSymbol<FunctionOp>(op.getCallee());
-      assert(op.getArgs().size() == callee.getArgumentTypes().size());
-      auto pairs = llvm::zip(op.getArgs(), callee.getArgumentTypes());
-
-      for (auto [ arg, type ] : pairs) {
-        mlir::Type actualType = arg.getType();
-
-        if (!actualType.isa<ArrayType>() && !type.isa<ArrayType>()) {
-          continue;
-        }
-
-        if (!actualType.isa<ArrayType>() && type.isa<ArrayType>()) {
-          return mlir::failure();
-        }
-
-        if (actualType.isa<ArrayType>() && !type.isa<ArrayType>()) {
-          return mlir::failure();
-        }
-
-        if (actualType.cast<ArrayType>().getRank() != type.cast<ArrayType>().getRank()) {
-          return mlir::failure();
-        }
+    public:
+      CallOpScalarPattern(
+          mlir::MLIRContext* context, mlir::SymbolTableCollection& symbolTable)
+          : mlir::OpRewritePattern<CallOp>(context),
+            symbolTable(&symbolTable)
+      {
       }
 
-      return mlir::success();
-    }
+      mlir::LogicalResult matchAndRewrite(
+          CallOp op, mlir::PatternRewriter& rewriter) const override
+      {
+        auto module = op->getParentOfType<mlir::ModuleOp>();
 
-    void rewrite(CallOp op, mlir::PatternRewriter& rewriter) const override
-    {
-      mlir::Location location = op->getLoc();
+        auto calleeName =
+            mlir::StringAttr::get(op.getContext(), op.getCallee());
 
-      auto module = op->getParentOfType<mlir::ModuleOp>();
-      auto callee = module.lookupSymbol<FunctionOp>(op.getCallee());
+        auto calleeFunctionOp =
+            symbolTable->lookupSymbolIn<FunctionOp>(module, calleeName);
 
-      llvm::SmallVector<mlir::Value, 3> args;
+        assert(op.getArgs().size() ==
+               calleeFunctionOp.getArgumentTypes().size());
 
-      for (auto [ arg, type ] : llvm::zip(op.getArgs(), callee.getArgumentTypes())) {
-        if (arg.getType() != type) {
-          if (arg.getType().isa<ArrayType>()) {
-            arg = rewriter.create<ArrayCastOp>(location, type, arg);
-          } else {
-            arg = rewriter.create<CastOp>(location, type, arg);
+        auto pairs = llvm::zip(
+            op.getArgs(), calleeFunctionOp.getArgumentTypes());
+
+        for (auto [ arg, type ] : pairs) {
+          mlir::Type actualType = arg.getType();
+
+          if (!actualType.isa<ArrayType>() && !type.isa<ArrayType>()) {
+            continue;
+          }
+
+          if (!actualType.isa<ArrayType>() && type.isa<ArrayType>()) {
+            return mlir::failure();
+          }
+
+          if (actualType.isa<ArrayType>() && !type.isa<ArrayType>()) {
+            return mlir::failure();
+          }
+
+          if (actualType.cast<ArrayType>().getRank() !=
+              type.cast<ArrayType>().getRank()) {
+            return mlir::failure();
           }
         }
 
-        args.push_back(arg);
+        mlir::Location location = op->getLoc();
+        llvm::SmallVector<mlir::Value, 3> args;
+
+        for (auto [ arg, type ] : llvm::zip(
+                 op.getArgs(), calleeFunctionOp.getArgumentTypes())) {
+          if (arg.getType() != type) {
+            if (arg.getType().isa<ArrayType>()) {
+              arg = rewriter.create<ArrayCastOp>(location, type, arg);
+            } else {
+              arg = rewriter.create<CastOp>(location, type, arg);
+            }
+          }
+
+          args.push_back(arg);
+        }
+
+        auto movedResults = op.getMovedResults();
+
+        if (movedResults.has_value()) {
+          rewriter.replaceOpWithNewOp<CallOp>(
+              op,
+              op.getResultTypes(),
+              op.getCallee(),
+              args,
+              rewriter.getI64IntegerAttr(movedResults->getSExtValue()));
+        } else {
+          rewriter.replaceOpWithNewOp<CallOp>(
+              op, op.getCallee(), op.getResultTypes(), args);
+        }
+
+        return mlir::success();
       }
 
-      auto movedResults = op.getMovedResults();
-
-      if (movedResults.has_value()) {
-        rewriter.replaceOpWithNewOp<CallOp>(op, op.getResultTypes(), op.getCallee(), args, rewriter.getI64IntegerAttr(movedResults->getSExtValue()));
-      } else {
-        rewriter.replaceOpWithNewOp<CallOp>(op, op.getCallee(), op.getResultTypes(), args);
-      }
-    }
+    private:
+      mlir::SymbolTableCollection* symbolTable;
   };
 
   struct SubscriptionOpPattern : public mlir::OpRewritePattern<SubscriptionOp>
   {
     using mlir::OpRewritePattern<SubscriptionOp>::OpRewritePattern;
 
-    mlir::LogicalResult matchAndRewrite(SubscriptionOp op, mlir::PatternRewriter& rewriter) const override
+    mlir::LogicalResult matchAndRewrite(
+        SubscriptionOp op, mlir::PatternRewriter& rewriter) const override
     {
       mlir::Location location = op->getLoc();
       llvm::SmallVector<mlir::Value, 3> indexes;
 
       for (mlir::Value index : op.getIndices()) {
         if (!index.getType().isa<mlir::IndexType>()) {
-          index = rewriter.create<CastOp>(location, rewriter.getIndexType(), index);
+          index = rewriter.create<CastOp>(
+              location, rewriter.getIndexType(), index);
         }
 
         indexes.push_back(index);
@@ -105,10 +127,12 @@ namespace
   {
     using mlir::OpRewritePattern<ConditionOp>::OpRewritePattern;
 
-    mlir::LogicalResult matchAndRewrite(ConditionOp op, mlir::PatternRewriter& rewriter) const override
+    mlir::LogicalResult matchAndRewrite(
+        ConditionOp op, mlir::PatternRewriter& rewriter) const override
     {
       mlir::Location location = op->getLoc();
-      mlir::Value condition = rewriter.create<CastOp>(location, BooleanType::get(op.getContext()), op.getCondition());
+      mlir::Value condition = rewriter.create<CastOp>(
+          location, BooleanType::get(op.getContext()), op.getCondition());
       rewriter.replaceOpWithNewOp<ConditionOp>(op, condition);
       return mlir::success();
     }
@@ -116,51 +140,21 @@ namespace
 }
 
 static void populateExplicitCastInsertionPatterns(
-    mlir::RewritePatternSet& patterns, mlir::MLIRContext* context)
+    mlir::RewritePatternSet& patterns,
+    mlir::MLIRContext* context,
+    mlir::SymbolTableCollection& symbolTable)
 {
-	patterns.insert<
-	    CallOpScalarPattern,
-			SubscriptionOpPattern,
-			ConditionOpPattern>(context);
+	patterns.insert<CallOpScalarPattern>(context, symbolTable);
+
+  patterns.insert<
+      SubscriptionOpPattern,
+      ConditionOpPattern>(context);
 }
 
 namespace
 {
-  struct ExplicitCastInsertionTarget : public mlir::ConversionTarget {
-    ExplicitCastInsertionTarget(mlir::MLIRContext& context) : mlir::ConversionTarget(context)
-    {
-      addDynamicallyLegalOp<CallOp>([](CallOp op) {
-        auto module = op->getParentOfType<mlir::ModuleOp>();
-        auto callee = module.lookupSymbol<FunctionOp>(op.getCallee());
-
-        if (callee == nullptr) {
-          return true;
-        }
-
-        assert(op.getArgs().size() == callee.getArgumentTypes().size());
-        auto pairs = llvm::zip(op.getArgs(), callee.getArgumentTypes());
-
-        return llvm::all_of(pairs, [&](const auto& pair) {
-          return std::get<0>(pair).getType() == std::get<1>(pair);
-        });
-      });
-
-      addDynamicallyLegalOp<SubscriptionOp>([](SubscriptionOp op) {
-        auto indexes = op.getIndices();
-
-        return llvm::all_of(indexes, [](mlir::Value index) {
-          return index.getType().isa<mlir::IndexType>();
-        });
-      });
-
-      addDynamicallyLegalOp<ConditionOp>([](ConditionOp op) {
-        mlir::Type conditionType = op.getCondition().getType();
-        return conditionType.isa<BooleanType>();
-      });
-    }
-  };
-
-  class ExplicitCastInsertionPass: public impl::ExplicitCastInsertionPassBase<ExplicitCastInsertionPass>
+  class ExplicitCastInsertionPass
+      : public impl::ExplicitCastInsertionPassBase<ExplicitCastInsertionPass>
   {
     public:
       using ExplicitCastInsertionPassBase::ExplicitCastInsertionPassBase;
@@ -169,16 +163,62 @@ namespace
       {
         auto module = getOperation();
 
-        ExplicitCastInsertionTarget target(getContext());
+        mlir::ConversionTarget target(getContext());
         target.addLegalDialect<ModelicaDialect>();
         target.addLegalDialect<mlir::scf::SCFDialect>();
 
-        mlir::RewritePatternSet patterns(&getContext());
-        populateExplicitCastInsertionPatterns(patterns, &getContext());
+        // Create an instance of the symbol table in order to reduce the cost
+        // of looking for symbols.
+        mlir::SymbolTableCollection symbolTable;
 
-        if (failed(applyPartialConversion(module, target, std::move(patterns)))) {
-          mlir::emitError(module.getLoc(), "Error in inserting the explicit casts");
-          signalPassFailure();
+        target.addDynamicallyLegalOp<CallOp>([&](CallOp op) {
+          auto module = op->getParentOfType<mlir::ModuleOp>();
+
+          auto calleeName =
+              mlir::StringAttr::get(op.getContext(), op.getCallee());
+
+          auto calleeFunctionOp =
+              symbolTable.lookupSymbolIn<FunctionOp>(module, calleeName);
+
+          if (calleeFunctionOp == nullptr) {
+            return true;
+          }
+
+          assert(op.getArgs().size() ==
+                 calleeFunctionOp.getArgumentTypes().size());
+
+          auto pairs = llvm::zip(
+              op.getArgs(), calleeFunctionOp.getArgumentTypes());
+
+          return llvm::all_of(pairs, [&](const auto& pair) {
+            return std::get<0>(pair).getType() == std::get<1>(pair);
+          });
+        });
+
+        target.addDynamicallyLegalOp<SubscriptionOp>([](SubscriptionOp op) {
+          auto indexes = op.getIndices();
+
+          return llvm::all_of(indexes, [](mlir::Value index) {
+            return index.getType().isa<mlir::IndexType>();
+          });
+        });
+
+        target.addDynamicallyLegalOp<ConditionOp>([](ConditionOp op) {
+          mlir::Type conditionType = op.getCondition().getType();
+          return conditionType.isa<BooleanType>();
+        });
+
+        mlir::RewritePatternSet patterns(&getContext());
+
+        populateExplicitCastInsertionPatterns(
+            patterns, &getContext(), symbolTable);
+
+        if (mlir::failed(applyPartialConversion(
+                module, target, std::move(patterns)))) {
+          mlir::emitError(
+              module.getLoc(), "Error in inserting the explicit casts");
+
+          return signalPassFailure();
         }
       }
   };
