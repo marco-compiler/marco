a01af26c2e218b3f77d6a82307ea724859818c2b
Michele Scuttari <michele.scuttari@outlook.com>
2023-11-04 13:45:38 +0100

    Minor improvements to the test suite description file

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -2,25 +2,33 @@
 
 import os
 import sys
+
 import lit.util
 
 from lit.llvm import llvm_config
+from lit.llvm.subst import ToolSubst
+from lit.llvm.subst import FindTool
 
 # Configuration file for the 'lit' test runner.
 
-# name: The name of this test suite.
+# Name of the test suite.
 config.name = 'MARCO'
 
-# suffixes: A list of file extensions to treat as test files.
-config.suffixes = ['.cpp', '.mo', '.mlir', '.test']
+# List of file extensions to treat as test files.
+config.suffixes = [
+    ".cpp",
+    ".mo",
+    ".mlir",
+    ".test"
+]
 
-# test_format: The test format to use to interpret tests.
+# Test format to use to interpret tests.
 config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
 
-# test_source_root: The root path where tests are located.
+# Root path where tests are located.
 config.test_source_root = os.path.dirname(__file__)
 
-# test_exec_root: The root path where tests should be run.
+# Root path where tests should be run.
 config.test_exec_root = os.path.join(config.marco_obj_root, 'test')
 
 # On MacOS, set the environment variable for the path of the SDK to be used.
@@ -28,27 +36,37 @@ lit.util.usePlatformSdkOnDarwin(config, lit_config)
 
 config.substitutions.append(('%PATH%', config.environment['PATH']))
 config.substitutions.append(('%shlibext', config.llvm_shlib_ext))
+
+# Set the path containing the runtime library.
 config.substitutions.append(('%runtime_lib_dir', config.marco_runtime_lib_dir))
 
+# Check if the runtime library has been found.
+# If not, the simulations can't be run.
 if config.marco_runtime_found == "1":
     config.available_features.add("runtime-library")
 
+# Check if the IDA solver is enabled.
 if config.marco_runtime_ida_enabled == "ON":
     config.available_features.add("runtime-ida")
 
+# Set the path containing the SUNDIALS libraries.
 config.substitutions.append(("%sundials_lib_dir", config.marco_runtime_sundials_lib_dir))
 
-llvm_config.with_system_environment(['HOME', 'INCLUDE', 'LIB', 'TMP', 'TEMP'])
+llvm_config.with_system_environment(["HOME", "INCLUDE", "LIB", "TMP", "TEMP"])
 llvm_config.use_default_substitutions()
 
-# excludes: A list of directories to exclude from the testsuite. The 'Inputs'
-# subdirectories contain auxiliary inputs for various tests in their parent
-# directories.
-config.excludes = ['Inputs', 'Examples', 'CMakeLists.txt', 'README.txt', 'LICENSE.txt']
+# List of directories to exclude from the testsuite.
+config.excludes = [
+    "CMakeLists.txt",
+    "README.txt",
+    "LICENSE.txt",
+    "lit.cfg.py",
+    "lit.site.cfg.py"
+]
 
 # Tweak the PATH to include the tools dir.
-path = os.path.pathsep.join((config.marco_tools_dir, config.llvm_tools_dir, config.environment['PATH']))
-config.environment['PATH'] = path
+llvm_config.with_environment("PATH", config.marco_tools_dir, append_path=True)
+llvm_config.with_environment("PATH", config.llvm_tools_dir, append_path=True)
 
 tool_dirs = [
     config.marco_tools_dir,
@@ -56,9 +74,9 @@ tool_dirs = [
 ]
 
 tools = [
-    'mlir-cpu-runner',
-    'marco',
-    'modelica-opt'
+    "mlir-cpu-runner",
+    "marco",
+    "modelica-opt"
 ]
 
 llvm_config.add_tool_substitutions(tools, tool_dirs)
@@ -67,6 +85,6 @@ llvm_config.add_tool_substitutions(tools, tool_dirs)
 ld_library_path = os.path.pathsep.join((
     config.marco_libs_dir,
     config.llvm_libs_dir,
-    config.environment.get('LD_LIBRARY_PATH','')))
+    config.environment.get("LD_LIBRARY_PATH", "")))
 
-config.environment['LD_LIBRARY_PATH'] = ld_library_path
+config.environment["LD_LIBRARY_PATH"] = ld_library_path
