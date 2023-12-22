4e458f7b4534e8ecd4d270f5daa0385c42dfd30e
Michele Scuttari <michele.scuttari@outlook.com>
2023-12-22 21:58:26 +0100

    Add SUNDIALS dialect dependency to IDA and KINSOL dialects

--- include/public/marco/Dialect/KINSOL/KINSOLDialect.h
+++ include/public/marco/Dialect/KINSOL/KINSOLDialect.h
@@ -1,11 +1,12 @@
 #ifndef MARCO_DIALECTS_KINSOL_KINSOLDIALECT_H
 #define MARCO_DIALECTS_KINSOL_KINSOLDIALECT_H
 
-#include "mlir/IR/Dialect.h"
-#include "mlir/Interfaces/SideEffectInterfaces.h"
+#include "marco/Dialect/SUNDIALS/SUNDIALSDialect.h"
 #include "marco/Dialect/KINSOL/Attributes.h"
 #include "marco/Dialect/KINSOL/Ops.h"
 #include "marco/Dialect/KINSOL/Types.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
 
 #include "marco/Dialect/KINSOL/KINSOLDialect.h.inc"
 
