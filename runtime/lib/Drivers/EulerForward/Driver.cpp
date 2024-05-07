ff28e250243e0f9d85a7d907f33c78107f3c774c
Michele Scuttari <michele.scuttari@outlook.com>
2024-05-07 09:35:33 +0200

    Add debug messages to Euler forward driver

--- runtime/lib/Drivers/EulerForward/Driver.cpp
+++ runtime/lib/Drivers/EulerForward/Driver.cpp
@@ -5,6 +5,7 @@
 #include "marco/Runtime/Simulation/Options.h"
 #include "marco/Runtime/Simulation/Profiler.h"
 #include "marco/Runtime/Simulation/Runtime.h"
+#include <iostream>
 
 namespace marco::runtime
 {
@@ -22,15 +23,27 @@ namespace marco::runtime
 
   int EulerForward::run()
   {
+    if (marco::runtime::simulation::getOptions().debug) {
+      std::cerr << "[Euler Forward] Starting simulation" << std::endl;
+    }
+
     double time;
 
     do {
       // Compute the next values of the state variables.
+      if (marco::runtime::simulation::getOptions().debug) {
+        std::cerr << "[Euler Forward] Updating state variables" << std::endl;
+      }
+
       EULER_FORWARD_PROFILER_STATEVAR_START;
       updateStateVariables(eulerforward::getOptions().timeStep);
       EULER_FORWARD_PROFILER_STATEVAR_STOP;
 
       // Move to the next step.
+      if (marco::runtime::simulation::getOptions().debug) {
+        std::cerr << "[Euler Forward] Updating time and non-state variables" << std::endl;
+      }
+
       EULER_FORWARD_PROFILER_NONSTATEVAR_START;
       time = getTime() + eulerforward::getOptions().timeStep;
       setTime(time);
@@ -38,10 +51,18 @@ namespace marco::runtime
       updateNonStateVariables();
       EULER_FORWARD_PROFILER_NONSTATEVAR_STOP;
 
+      if (marco::runtime::simulation::getOptions().debug) {
+        std::cerr << "[Euler Forward] Printing values" << std::endl;
+      }
+
       // Print the values.
       getSimulation()->getPrinter()->printValues();
     } while (std::abs(simulation::getOptions().endTime - time) >=
              eulerforward::getOptions().timeStep);
+
+    if (marco::runtime::simulation::getOptions().debug) {
+      std::cerr << "[Euler Forward] Simulation finished" << std::endl;
+    }
 
     return EXIT_SUCCESS;
   }
