440d6c9beb4c00e06825cc6f1a78dd52a1b5ed0f
gianvitocaleca <gianvito.caleca@mail.polimi.it>
2024-05-07 15:56:46 +0200

    Better error message in LogOp

--- lib/Dialect/BaseModelica/IR/Ops.cpp
+++ lib/Dialect/BaseModelica/IR/Ops.cpp
@@ -6769,11 +6769,13 @@ namespace mlir::bmodelica
 
     mlir::Value zero = builder.create<mlir::arith::ConstantOp>(
         loc, builder.getI64IntegerAttr(0));
+  
     mlir::Value condition = builder.create<mlir::arith::CmpIOp>(
         loc, mlir::arith::CmpIPredicate::sgt, argCast, zero);
 
     builder.create<mlir::cf::AssertOp>(
-        loc, condition, builder.getStringAttr("Invalid argument"));
+        loc, condition, builder.getStringAttr(
+          "Model error: Argument of log outside the domain. It should be > 0"));
   }
 }
 
