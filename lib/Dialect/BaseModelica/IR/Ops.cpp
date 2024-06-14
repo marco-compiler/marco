02db95ef8a0fd0f0abcd3dff20212ccfbf9ee0ce
gianvitocaleca <gianvito.caleca@mail.polimi.it>
2024-06-14 14:34:56 +0200

    asin, acos, log10 runtime verification

--- lib/Dialect/BaseModelica/IR/Ops.cpp
+++ lib/Dialect/BaseModelica/IR/Ops.cpp
@@ -6708,7 +6708,36 @@ namespace mlir::bmodelica
 
     return {};
   }
+  void AcosOp::generateRuntimeVerification(
+      mlir::OpBuilder& builder, mlir::Location loc)
+  {
+    mlir::Value operand = getOperand();
+    // convert operand to arith-compatible type
+    mlir::Value argCast = builder.create<CastOp>(
+        loc, builder.getF64Type(), operand);
+
+    mlir::Value minusone = builder.create<mlir::arith::ConstantOp>(
+        loc, builder.getF64FloatAttr(-1));
+
+    mlir::Value one = builder.create<mlir::arith::ConstantOp>(
+        loc, builder.getF64FloatAttr(1));
+  
+    mlir::Value firstcondition = builder.create<mlir::arith::CmpFOp>(
+        loc, mlir::arith::CmpFPredicate::OGE, argCast, minusone);
+    
+    mlir::Value secondcondition = builder.create<mlir::arith::CmpFOp>(
+        loc, mlir::arith::CmpFPredicate::OLE, argCast, one);
+
+    builder.create<mlir::cf::AssertOp>(
+        loc, firstcondition, builder.getStringAttr(
+          "Model error: Argument of acos outside the domain. It should be -1 <= arg <= 1"));
+    
+    builder.create<mlir::cf::AssertOp>(
+        loc, secondcondition, builder.getStringAttr(
+          "Model error: Argument of acos outside the domain. It should be -1 <= arg <= 1"));
+  }
 }
+
 
 //===---------------------------------------------------------------------===//
 // AsinOp
@@ -6740,6 +6769,34 @@ namespace mlir::bmodelica
     }
 
     return {};
+  }
+  void AsinOp::generateRuntimeVerification(
+      mlir::OpBuilder& builder, mlir::Location loc)
+  {
+    mlir::Value operand = getOperand();
+    // convert operand to arith-compatible type
+    mlir::Value argCast = builder.create<CastOp>(
+        loc, builder.getF64Type(), operand);
+
+    mlir::Value minusone = builder.create<mlir::arith::ConstantOp>(
+        loc, builder.getF64FloatAttr(-1));
+
+    mlir::Value one = builder.create<mlir::arith::ConstantOp>(
+        loc, builder.getF64FloatAttr(1));
+  
+    mlir::Value firstcondition = builder.create<mlir::arith::CmpFOp>(
+        loc, mlir::arith::CmpFPredicate::OGE, argCast, minusone);
+    
+    mlir::Value secondcondition = builder.create<mlir::arith::CmpFOp>(
+        loc, mlir::arith::CmpFPredicate::OLE, argCast, one);
+
+    builder.create<mlir::cf::AssertOp>(
+        loc, firstcondition, builder.getStringAttr(
+          "Model error: Argument of asin outside the domain. It should be -1 <= arg <= 1"));
+    
+    builder.create<mlir::cf::AssertOp>(
+        loc, secondcondition, builder.getStringAttr(
+          "Model error: Argument of asin outside the domain. It should be -1 <= arg <= 1"));
   }
 }
 
@@ -6916,6 +6973,24 @@ namespace mlir::bmodelica
     }
 
     return {};
+  }
+  void DivTruncOp::generateRuntimeVerification(
+      mlir::OpBuilder& builder, mlir::Location loc)
+  {
+    mlir::Value operand = getOperand(1);
+    // convert operand to arith-compatible type
+    mlir::Value argCast = builder.create<CastOp>(
+        loc, builder.getF64Type(), operand);
+
+    mlir::Value zero = builder.create<mlir::arith::ConstantOp>(
+        loc, builder.getF64FloatAttr(0));
+  
+    mlir::Value condition = builder.create<mlir::arith::CmpFOp>(
+        loc, mlir::arith::CmpFPredicate::ONE, argCast, zero);
+
+    builder.create<mlir::cf::AssertOp>(
+        loc, condition, builder.getStringAttr(
+          "Model error: division by zero"));
   }
 }
 
@@ -7094,6 +7169,24 @@ namespace mlir::bmodelica
     }
 
     return {};
+  }
+  void Log10Op::generateRuntimeVerification(
+      mlir::OpBuilder& builder, mlir::Location loc)
+  {
+    mlir::Value operand = getOperand();
+    // convert operand to arith-compatible type
+    mlir::Value argCast = builder.create<CastOp>(
+        loc, builder.getF64Type(), operand);
+
+    mlir::Value zero = builder.create<mlir::arith::ConstantOp>(
+        loc, builder.getF64FloatAttr(0));
+  
+    mlir::Value condition = builder.create<mlir::arith::CmpFOp>(
+        loc, mlir::arith::CmpFPredicate::OGT, argCast, zero);
+
+    builder.create<mlir::cf::AssertOp>(
+        loc, condition, builder.getStringAttr(
+          "Model error: Argument of log10 outside the domain. It should be > 0"));
   }
 }
 
