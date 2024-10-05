86701e8cf649789d3cde3b0f4781ffe92c484932
Michele Scuttari <michele.scuttari@outlook.com>
2024-10-05 19:01:23 +0200

    Format code

--- include/public/marco/Frontend/CodegenOptions.h
+++ include/public/marco/Frontend/CodegenOptions.h
@@ -6,36 +6,34 @@
 #include <string>
 #include <vector>
 
-namespace marco::frontend
-{
-  /// Code generation operations.
-  /// The default values are for compiling without optimizations.
-  /// The class extends the language options for C / C++ to enable the
-  /// integration with clang's diagnostics infrastructure.
-  struct CodegenOptions : public clang::CodeGenOptions
-  {
-    llvm::OptimizationLevel optLevel = llvm::OptimizationLevel::O0;
+namespace marco::frontend {
+/// Code generation operations.
+/// The default values are for compiling without optimizations.
+/// The class extends the language options for C / C++ to enable the
+/// integration with clang's diagnostics infrastructure.
+struct CodegenOptions : public clang::CodeGenOptions {
+  llvm::OptimizationLevel optLevel = llvm::OptimizationLevel::O0;
 
-    bool debug = true;
-    bool assertions = true;
-    bool inlining = false;
-    bool outputArraysPromotion = false;
-    bool heapToStackPromotion = false;
-    bool readOnlyVariablesPropagation = false;
-    bool variablesToParametersPromotion = false;
-    bool cse = false;
-    bool equationsRuntimeScheduling = false;
-    bool omp = false;
-    bool singleValuedInductionElimination = false;
-    bool loopFusion = false;
-    bool loopCoalescing = false;
-    bool loopTiling = false;
+  bool debug = true;
+  bool assertions = true;
+  bool inlining = false;
+  bool outputArraysPromotion = false;
+  bool heapToStackPromotion = false;
+  bool readOnlyVariablesPropagation = false;
+  bool variablesToParametersPromotion = false;
+  bool cse = false;
+  bool equationsRuntimeScheduling = false;
+  bool omp = false;
+  bool singleValuedInductionElimination = false;
+  bool loopFusion = false;
+  bool loopCoalescing = false;
+  bool loopTiling = false;
 
-    unsigned int bitWidth = 64;
+  unsigned int bitWidth = 64;
 
-    std::string cpu = "generic";
-    std::vector<std::string> features;
-  };
-}
+  std::string cpu = "generic";
+  std::vector<std::string> features;
+};
+} // namespace marco::frontend
 
 #endif // MARCO_FRONTEND_CODEGENOPTIONS_H
