e610736375001868f46903248d5cef80f9aeb4b2
Arran Gabriel <arrankg@gmail.com>
2024-10-15 16:47:32 +0200

    Register statistics and resolve style issues

--- lib/Dialect/BaseModelica/Transforms/CallCSE.cpp
+++ lib/Dialect/BaseModelica/Transforms/CallCSE.cpp
@@ -1,8 +1,6 @@
 #include "marco/Dialect/BaseModelica/Transforms/CallCSE.h"
 #include "marco/Dialect/BaseModelica/IR/BaseModelica.h"
 
-#include <marco/AST/Node/Operation.h>
-
 namespace mlir::bmodelica {
 #define GEN_PASS_DEF_CALLCSEPASS
 #include "marco/Dialect/BaseModelica/Transforms/Passes.h.inc"
@@ -13,56 +11,23 @@ using namespace ::mlir::bmodelica;
 namespace {
 class CallCSEPass final : public impl::CallCSEPassBase<CallCSEPass> {
 public:
-  using CallCSEPassBase::CallCSEPassBase;
+  using CallCSEPassBase<CallCSEPass>::CallCSEPassBase;
 
   void runOnOperation() override;
 
 private:
-  static mlir::LogicalResult processModelOp(ModelOp modelOp);
-
-  /// Get all callOps in the model.
-  static void collectCallOps(ModelOp modelOp,
-                             llvm::SmallVector<CallOp> &callOps);
-
-  /// Partition the list of call operations into groups given by
-  /// EquationExpressionOpInterface::isEquivalent
-  static void buildCallEquivalenceGroups(
-      llvm::SmallVector<CallOp> &callOps,
-      mlir::SymbolTableCollection &symbolTableCollection,
-      llvm::SmallVector<llvm::SmallVector<CallOp>> &callEquivalenceGroups);
-
-  static mlir::Operation *cloneDefUseChain(mlir::Operation *op,
-                                           mlir::IRMapping &mapping,
-                                           mlir::RewriterBase &rewriter);
+  mlir::LogicalResult processModelOp(ModelOp modelOp);
 
   /// Replace all calls in the equivalence group with gets to a generated
   /// variable. The variable will be driven by an equation derived from the
   /// first call in the group.
-  static EquationTemplateOp emitCse(ModelOp modelOp, int emittedCSEs,
-                                    llvm::SmallVector<CallOp> &equivalenceGroup,
-                                    mlir::RewriterBase &rewriter);
+  EquationTemplateOp emitCse(ModelOp modelOp, int emittedCSEs,
+                             llvm::SmallVectorImpl<CallOp> &equivalenceGroup,
+                             mlir::RewriterBase &rewriter);
 };
-} // namespace
 
-void CallCSEPass::runOnOperation() {
-  llvm::SmallVector<ModelOp, 1> modelOps;
-
-  walkClasses(getOperation(), [&](mlir::Operation *op) {
-    if (auto modelOp = mlir::dyn_cast<ModelOp>(op)) {
-      modelOps.push_back(modelOp);
-    }
-  });
-
-  if (failed(failableParallelForEach(
-          &getContext(), modelOps, [&](mlir::Operation *op) {
-            return processModelOp(mlir::cast<ModelOp>(op));
-          }))) {
-    return signalPassFailure();
-  }
-}
-
-void CallCSEPass::collectCallOps(ModelOp modelOp,
-                                 llvm::SmallVector<CallOp> &callOps) {
+/// Get all callOps in the model.
+void collectCallOps(ModelOp modelOp, llvm::SmallVectorImpl<CallOp> &callOps) {
   llvm::SmallVector<EquationInstanceOp> initialEquationOps;
   llvm::SmallVector<EquationInstanceOp> dynamicEquationOps;
 
@@ -89,38 +54,48 @@ void CallCSEPass::collectCallOps(ModelOp modelOp,
   }
 }
 
-void CallCSEPass::buildCallEquivalenceGroups(
-    llvm::SmallVector<CallOp> &callOps,
-    mlir::SymbolTableCollection &symbolTableCollection,
-    llvm::SmallVector<llvm::SmallVector<CallOp>> &callEquivalenceGroups) {
+/// Partition the list of call operations into groups given by
+/// EquationExpressionOpInterface::isEquivalent
+void buildCallEquivalenceGroups(
+    llvm::SmallVectorImpl<CallOp> &callOps,
+    llvm::SmallVectorImpl<llvm::SmallVector<CallOp>> &callEquivalenceGroups) {
+  mlir::SymbolTableCollection symbolTableCollection;
+  llvm::SmallVector<llvm::SmallVector<CallOp>> tmpCallEquivalenceGroups;
+
   for (auto callOp : callOps) {
     auto callExpression =
         mlir::cast<EquationExpressionOpInterface>(callOp.getOperation());
 
-    auto *equivalenceGroup =
-        find_if(callEquivalenceGroups, [&](llvm::SmallVector<CallOp> &group) {
-          // front() is safe as there are no empty groups
-          auto representative = mlir::cast<EquationExpressionOpInterface>(
-              group.front().getOperation());
-          return callExpression.isEquivalent(representative,
+    auto *equivalenceGroup = find_if(
+        tmpCallEquivalenceGroups, [&](llvm::SmallVector<CallOp> &group) {
+          assert(!group.empty() && "groups should never be empty");
+          return callExpression.isEquivalent(group.front(),
                                              symbolTableCollection);
         });
 
-    if (equivalenceGroup != callEquivalenceGroups.end()) {
+    if (equivalenceGroup != tmpCallEquivalenceGroups.end()) {
       // Add equivalent call to existing group
       equivalenceGroup->push_back(callOp);
     } else {
       // Create new equivalence group
-      callEquivalenceGroups.push_back({callOp});
+      tmpCallEquivalenceGroups.push_back({callOp});
+    }
+  }
+
+  for (auto &group : tmpCallEquivalenceGroups) {
+    if (group.size() > 1) {
+      callEquivalenceGroups.push_back(group);
     }
   }
 }
 
-mlir::Operation *CallCSEPass::cloneDefUseChain(mlir::Operation *op,
-                                               mlir::IRMapping &mapping,
-                                               mlir::RewriterBase &rewriter) {
-  std::vector<mlir::Operation *> toClone;
-  std::vector worklist({op});
+/// Clone `op` and its def-use chain, returning the cloned version of `op`.
+mlir::Operation *cloneDefUseChain(mlir::Operation *op,
+                                  mlir::RewriterBase &rewriter) {
+  mlir::IRMapping mapping;
+  // TODO - handle regions
+  llvm::SmallVector<mlir::Operation *> toClone;
+  llvm::SmallVector<mlir::Operation *> worklist({op});
 
   // DFS through the def-use chain of `op`
   while (!worklist.empty()) {
@@ -143,7 +118,7 @@ mlir::Operation *CallCSEPass::cloneDefUseChain(mlir::Operation *op,
 
 EquationTemplateOp
 CallCSEPass::emitCse(ModelOp modelOp, const int emittedCSEs,
-                     llvm::SmallVector<CallOp> &equivalenceGroup,
+                     llvm::SmallVectorImpl<CallOp> &equivalenceGroup,
                      mlir::RewriterBase &rewriter) {
   assert(!equivalenceGroup.empty() && "equivalenceGroup cannot be empty");
   auto representative = equivalenceGroup.front();
@@ -159,14 +134,11 @@ CallCSEPass::emitCse(ModelOp modelOp, const int emittedCSEs,
   auto equationTemplateOp = rewriter.create<EquationTemplateOp>(loc);
   rewriter.setInsertionPointToStart(equationTemplateOp.createBody(0));
 
-  mlir::IRMapping mapping;
-  auto *clonedRepresentative =
-      cloneDefUseChain(representative, mapping, rewriter);
+  auto lhsOp = rewriter.create<EquationSideOp>(
+      loc, rewriter.create<VariableGetOp>(loc, cseVariable)->getResults());
 
-  auto cseGetOp = rewriter.create<VariableGetOp>(loc, cseVariable);
-  auto lhsOp = rewriter.create<EquationSideOp>(loc, cseGetOp->getResults());
-  auto rhsOp =
-      rewriter.create<EquationSideOp>(loc, clonedRepresentative->getResults());
+  auto rhsOp = rewriter.create<EquationSideOp>(
+      loc, cloneDefUseChain(representative, rewriter)->getResults());
   rewriter.create<EquationSidesOp>(loc, lhsOp, rhsOp);
 
   // Replace calls with get to CSE variable
@@ -174,43 +146,61 @@ CallCSEPass::emitCse(ModelOp modelOp, const int emittedCSEs,
     rewriter.setInsertionPoint(callOp);
     rewriter.replaceOpWithNewOp<VariableGetOp>(callOp, cseVariable);
   }
+  this->replacedCalls += equivalenceGroup.size();
+  ++this->emittedCSEs;
 
   return equationTemplateOp;
 }
 
 mlir::LogicalResult CallCSEPass::processModelOp(ModelOp modelOp) {
   mlir::IRRewriter rewriter(modelOp);
-  mlir::SymbolTableCollection symbolTableCollection;
 
   llvm::SmallVector<CallOp> callOps;
   collectCallOps(modelOp, callOps);
 
   llvm::SmallVector<llvm::SmallVector<CallOp>> callEquivalenceGroups;
-  buildCallEquivalenceGroups(callOps, symbolTableCollection,
-                             callEquivalenceGroups);
+  buildCallEquivalenceGroups(callOps, callEquivalenceGroups);
+
+  if (callEquivalenceGroups.empty()) {
+    return mlir::success();
+  }
 
   int emittedCSEs = 0;
   llvm::SmallVector<EquationTemplateOp> cseEquationTemplateOps;
-
-  for (auto &equivalenceGroup :
-       make_filter_range(callEquivalenceGroups,
-                         [](auto &group) { return group.size() > 1; })) {
+  for (auto &equivalenceGroup : callEquivalenceGroups) {
     cseEquationTemplateOps.push_back(
         emitCse(modelOp, emittedCSEs++, equivalenceGroup, rewriter));
   }
 
-  if (!cseEquationTemplateOps.empty()) {
-    rewriter.setInsertionPointToEnd(modelOp.getBody());
-    auto dynamicOp = rewriter.create<DynamicOp>(rewriter.getUnknownLoc());
-    rewriter.setInsertionPointToStart(
-        rewriter.createBlock(&dynamicOp.getRegion()));
-    for (auto &equationTemplateOp : cseEquationTemplateOps) {
-      rewriter.create<EquationInstanceOp>(rewriter.getUnknownLoc(),
-                                          equationTemplateOp);
-    }
+  rewriter.setInsertionPointToEnd(modelOp.getBody());
+  auto dynamicOp = rewriter.create<DynamicOp>(rewriter.getUnknownLoc());
+  rewriter.setInsertionPointToStart(
+      rewriter.createBlock(&dynamicOp.getRegion()));
+
+  for (auto equationTemplateOp : cseEquationTemplateOps) {
+    rewriter.create<EquationInstanceOp>(rewriter.getUnknownLoc(),
+                                        equationTemplateOp);
   }
 
   return mlir::success();
+}
+} // namespace
+
+void CallCSEPass::runOnOperation() {
+  llvm::SmallVector<ModelOp, 1> modelOps;
+
+  walkClasses(getOperation(), [&](mlir::Operation *op) {
+    if (auto modelOp = mlir::dyn_cast<ModelOp>(op)) {
+      modelOps.push_back(modelOp);
+    }
+  });
+
+  if (mlir::failed(mlir::failableParallelForEach(
+          &getContext(), modelOps, [&](mlir::Operation *op) {
+            return processModelOp(mlir::cast<ModelOp>(op));
+          }))) {
+    return signalPassFailure();
+  }
 }
 
 namespace mlir::bmodelica {
