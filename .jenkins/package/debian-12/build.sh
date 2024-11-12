dc30778e5c1938b6baffbbc327be076895eb2a28
Michele Scuttari <michele.scuttari@outlook.com>
2024-11-12 23:41:23 +0100

    Avoid renaming Debian package

--- .jenkins/package/debian-12/build.sh
+++ .jenkins/package/debian-12/build.sh
@@ -3,30 +3,28 @@
 src_path=$1
 install_path=$2
 
-# Create folders.
-mkdir -p marco/DEBIAN
-mkdir -p marco/usr/bin
-mkdir -p marco/usr/lib/marco
-
-# Copy the control file.
-cp "${src_path}/.jenkins/package/debian-12/control" marco/DEBIAN/control
-
-# Copy the driver.
-cp "${install_path}/bin/marco" marco/usr/lib/marco/marco
-
-# Copy the driver wrapper.
-cp "${src_path}/.jenkins/package/debian-12/marco-wrapper.sh" marco/usr/bin/marco
-chmod +x marco/usr/bin/marco
-
-# Build the package.
-dpkg-deb --build marco
-
-# Clean the work directory.
-rm -rf marco
-
-# Extract version and architecture from the control file.
 version=$(grep "^Version:" "${src_path}/.jenkins/package/debian-12/control" | cut -d' ' -f2)
 architecture=$(grep "^Architecture:" "${src_path}/.jenkins/package/debian-12/control" | cut -d' ' -f2)
 
-# Rename the package.
-mv marco.deb marco-${version}_${architecture}.deb
+# Create folders.
+package_name=marco-${version}_${architecture}
+
+mkdir -p "${package_name}"/DEBIAN
+mkdir -p "${package_name}"/usr/bin
+mkdir -p "${package_name}"/usr/lib/marco
+
+# Copy the control file.
+cp "${src_path}/.jenkins/package/debian-12/control" "${package_name}"/DEBIAN/control
+
+# Copy the driver.
+cp "${install_path}/bin/marco" "${package_name}"/usr/lib/marco/marco
+
+# Copy the driver wrapper.
+cp "${src_path}/.jenkins/package/debian-12/marco-wrapper.sh" "${package_name}"/usr/bin/marco
+chmod +x "${package_name}"/usr/bin/marco
+
+# Build the package.
+dpkg-deb --build "${package_name}"
+
+# Clean the work directory.
+rm -rf "${package_name}"
