1b8483678fc8183111f0487af7cdd044f2e077a2
Michele Scuttari <michele.scuttari@outlook.com>
2024-11-13 18:59:59 +0100

    Fix linker flag in driver wrapping script

--- .jenkins/package/debian-12/marco-wrapper.sh
+++ .jenkins/package/debian-12/marco-wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Run the driver with the modified arguments.
-/usr/lib/marco/marco "$@" -L /usr/lib/marco-runtime -W,-rpath /usr/lib/marco-runtime
+/usr/lib/marco/marco "$@" -L /usr/lib/marco-runtime -Wl,-rpath /usr/lib/marco-runtime
