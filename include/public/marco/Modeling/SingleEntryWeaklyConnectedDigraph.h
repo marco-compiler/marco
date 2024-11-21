3ee4560cb1832928974eed281f10822a74e01d6b
Michele Scuttari <michele.scuttari@outlook.com>
2024-11-21 23:21:04 +0100

    Use unique_ptr directly instead of PtrProperty

--- include/public/marco/Modeling/SingleEntryWeaklyConnectedDigraph.h
+++ include/public/marco/Modeling/SingleEntryWeaklyConnectedDigraph.h
@@ -4,238 +4,165 @@
 #include "marco/Modeling/Graph.h"
 #include "llvm/ADT/GraphTraits.h"
 
-namespace marco::modeling::internal::dependency
-{
-  template<typename Property>
-  class PtrProperty
-  {
-    public:
-      PtrProperty() : property(nullptr)
-      {
-      }
+namespace marco::modeling::internal::dependency {
+/// A weakly connected directed graph with only one entry point.
+/// In other words, a directed graph that has an undirected path between
+/// any pair of vertices and has only one node with no predecessors.
+/// This is needed to work with the LLVM graph iterators, because a
+/// non-connected graph would lead to a visit of only the sub-graph
+/// containing the entry node.
+/// The single entry point also ensure the visit of all the nodes.
+/// The entry point is hidden from iteration upon vertices and can be
+/// accessed only by means of its dedicated getter.
+template <typename VP, typename EP = EmptyEdgeProperty>
+class SingleEntryWeaklyConnectedDigraph {
+public:
+  using VertexProperty = VP;
+  using EdgeProperty = EP;
 
-      explicit PtrProperty(Property property)
-          : property(std::make_unique<Property>(std::move(property)))
-      {
-      }
+private:
+  using Graph = DirectedGraph<std::unique_ptr<VertexProperty>,
+                              std::unique_ptr<EdgeProperty>>;
 
-      bool empty() const
-      {
-        return property == nullptr;
-      }
+public:
+  using VertexDescriptor = typename Graph::VertexDescriptor;
+  using EdgeDescriptor = typename Graph::EdgeDescriptor;
 
-      Property& operator*()
-      {
-        assert(!empty());
-        return *property;
-      }
+  using VertexIterator = typename Graph::VertexIterator;
+  using IncidentEdgeIterator = typename Graph::IncidentEdgeIterator;
+  using LinkedVerticesIterator = typename Graph::LinkedVerticesIterator;
 
-      const Property& operator*() const
-      {
-        assert(!empty());
-        return *property;
-      }
+  SingleEntryWeaklyConnectedDigraph() : entryNode(graph.addVertex(nullptr)) {}
 
-    private:
-      std::unique_ptr<Property> property;
-  };
+  VertexProperty &operator[](VertexDescriptor vertex) {
+    assert(vertex != entryNode && "The entry node doesn't have a property");
+    return *graph[vertex];
+  }
 
-  /// A weakly connected directed graph with only one entry point.
-  /// In other words, a directed graph that has an undirected path between
-  /// any pair of vertices and has only one node with no predecessors.
-  /// This is needed to work with the LLVM graph iterators, because a
-  /// non-connected graph would lead to a visit of only the sub-graph
-  /// containing the entry node.
-  /// The single entry point also ensure the visit of all the nodes.
-  /// The entry point is hidden from iteration upon vertices and can be
-  /// accessed only by means of its dedicated getter.
-  template<typename VP, typename EP = EmptyEdgeProperty>
-  class SingleEntryWeaklyConnectedDigraph
-  {
-    public:
-      using VertexProperty = VP;
-      using EdgeProperty = EP;
+  const VertexProperty &operator[](VertexDescriptor vertex) const {
+    assert(vertex != entryNode && "The entry node doesn't have a property");
+    return *graph[vertex];
+  }
 
-    private:
-      using Graph = DirectedGraph<
-          PtrProperty<VertexProperty>, PtrProperty<EdgeProperty>>;
+  EdgeProperty &operator[](EdgeDescriptor edge) { return *graph[edge]; }
 
-    public:
-      using VertexDescriptor = typename Graph::VertexDescriptor;
-      using EdgeDescriptor = typename Graph::EdgeDescriptor;
+  const EdgeProperty &operator[](EdgeDescriptor edge) const {
+    return *graph[edge];
+  }
 
-      using VertexIterator = typename Graph::VertexIterator;
-      using IncidentEdgeIterator = typename Graph::IncidentEdgeIterator;
-      using LinkedVerticesIterator = typename Graph::LinkedVerticesIterator;
+  size_t size() const { return graph.verticesCount(); }
 
-      SingleEntryWeaklyConnectedDigraph()
-          : entryNode(graph.addVertex(PtrProperty<VertexProperty>()))
-      {
-      }
+  VertexDescriptor getEntryNode() const { return entryNode; }
 
-      VertexProperty& operator[](VertexDescriptor vertex)
-      {
-        assert(vertex != entryNode && "The entry node doesn't have a property");
-        return *graph[vertex];
-      }
+  VertexDescriptor addVertex(VertexProperty property) {
+    auto descriptor =
+        graph.addVertex(std::make_unique<VertexProperty>(std::move(property)));
 
-      const VertexProperty& operator[](VertexDescriptor vertex) const
-      {
-        assert(vertex != entryNode && "The entry node doesn't have a property");
-        return *graph[vertex];
-      }
+    // Connect the entry node to the new vertex
+    graph.addEdge(entryNode, descriptor, nullptr);
 
-      EdgeProperty& operator[](EdgeDescriptor edge)
-      {
-        return *graph[edge];
-      }
+    return descriptor;
+  }
 
-      const EdgeProperty& operator[](EdgeDescriptor edge) const
-      {
-        return *graph[edge];
-      }
-
-      size_t size() const
-      {
-        return graph.verticesCount();
-      }
-
-      VertexDescriptor getEntryNode() const
-      {
-        return entryNode;
-      }
-
-      VertexDescriptor addVertex(VertexProperty property)
-      {
-        auto descriptor = graph.addVertex(PtrProperty(std::move(property)));
-
-        // Connect the entry node to the new vertex
-        graph.addEdge(entryNode, descriptor, PtrProperty<EdgeProperty>());
-
-        return descriptor;
-      }
-
-      auto verticesBegin() const
-      {
-        return graph.verticesBegin([](const typename Graph::VertexProperty& vertex) {
+  auto verticesBegin() const {
+    return graph.verticesBegin(
+        [](const typename Graph::VertexProperty &vertex) {
           // Hide the entry point
-          return !vertex.empty();
+          return vertex != nullptr;
         });
-      }
+  }
 
-      auto verticesEnd() const
-      {
-        return graph.verticesEnd([](const typename Graph::VertexProperty& vertex) {
-          // Hide the entry point
-          return !vertex.empty();
-        });
-      }
+  auto verticesEnd() const {
+    return graph.verticesEnd([](const typename Graph::VertexProperty &vertex) {
+      // Hide the entry point
+      return vertex != nullptr;
+    });
+  }
 
-      EdgeDescriptor addEdge(VertexDescriptor from, VertexDescriptor to, EdgeProperty property = EdgeProperty())
-      {
-        return graph.addEdge(from, to, PtrProperty(std::move(property)));
-      }
+  EdgeDescriptor addEdge(VertexDescriptor from, VertexDescriptor to,
+                         EdgeProperty property = EdgeProperty()) {
+    return graph.addEdge(from, to,
+                         std::make_unique<EdgeProperty>(std::move(property)));
+  }
 
-      auto getEdges() const
-      {
-        return graph.getEdges();
-      }
+  auto getEdges() const { return graph.getEdges(); }
 
-      auto outgoingEdgesBegin(VertexDescriptor vertex) const
-      {
-        return graph.outgoingEdgesBegin(std::move(vertex));
-      }
+  auto outgoingEdgesBegin(VertexDescriptor vertex) const {
+    return graph.outgoingEdgesBegin(std::move(vertex));
+  }
 
-      auto outgoingEdgesEnd(VertexDescriptor vertex) const
-      {
-        return graph.outgoingEdgesEnd(std::move(vertex));
-      }
+  auto outgoingEdgesEnd(VertexDescriptor vertex) const {
+    return graph.outgoingEdgesEnd(std::move(vertex));
+  }
 
-      auto linkedVerticesBegin(VertexDescriptor vertex) const
-      {
-        return graph.linkedVerticesBegin(std::move(vertex));
-      }
+  auto linkedVerticesBegin(VertexDescriptor vertex) const {
+    return graph.linkedVerticesBegin(std::move(vertex));
+  }
 
-      auto linkedVerticesEnd(VertexDescriptor vertex) const
-      {
-        return graph.linkedVerticesEnd(std::move(vertex));
-      }
+  auto linkedVerticesEnd(VertexDescriptor vertex) const {
+    return graph.linkedVerticesEnd(std::move(vertex));
+  }
 
-    private:
-      Graph graph;
-      VertexDescriptor entryNode;
-  };
-}
+private:
+  Graph graph;
+  VertexDescriptor entryNode;
+};
+} // namespace marco::modeling::internal::dependency
 
-namespace llvm
-{
-  // We specialize the LLVM's graph traits in order leverage the algorithms
-  // that are defined inside LLVM itself. This way we don't have to implement
-  // them from scratch.
-  template<typename VertexProperty>
-  struct GraphTraits<const marco::modeling::internal::dependency
-                     ::SingleEntryWeaklyConnectedDigraph<VertexProperty>*>
-  {
-    // The LLVM traits require the class specified as Graph to be copyable.
-    // We use its address to overcome this limitation.
-    using Graph = const marco::modeling::internal::dependency
-        ::SingleEntryWeaklyConnectedDigraph<VertexProperty>;
+namespace llvm {
+// We specialize the LLVM's graph traits in order leverage the algorithms
+// that are defined inside LLVM itself. This way we don't have to implement
+// them from scratch.
+template <typename VertexProperty>
+struct GraphTraits<const marco::modeling::internal::dependency ::
+                       SingleEntryWeaklyConnectedDigraph<VertexProperty> *> {
+  // The LLVM traits require the class specified as Graph to be copyable.
+  // We use its address to overcome this limitation.
+  using Graph = const marco::modeling::internal::dependency ::
+      SingleEntryWeaklyConnectedDigraph<VertexProperty>;
 
-    using GraphPtr = Graph*;
+  using GraphPtr = Graph *;
 
-    using NodeRef = typename Graph::VertexDescriptor;
-    using ChildIteratorType = typename Graph::LinkedVerticesIterator;
+  using NodeRef = typename Graph::VertexDescriptor;
+  using ChildIteratorType = typename Graph::LinkedVerticesIterator;
 
-    static NodeRef getEntryNode(const GraphPtr& graph)
-    {
-      return graph->getEntryNode();
-    }
+  static NodeRef getEntryNode(const GraphPtr &graph) {
+    return graph->getEntryNode();
+  }
 
-    static ChildIteratorType child_begin(NodeRef node)
-    {
-      return node.graph->linkedVerticesBegin(node);
-    }
+  static ChildIteratorType child_begin(NodeRef node) {
+    return node.graph->linkedVerticesBegin(node);
+  }
 
-    static ChildIteratorType child_end(NodeRef node)
-    {
-      return node.graph->linkedVerticesEnd(node);
-    }
+  static ChildIteratorType child_end(NodeRef node) {
+    return node.graph->linkedVerticesEnd(node);
+  }
 
-    using nodes_iterator = typename Graph::VertexIterator;
+  using nodes_iterator = typename Graph::VertexIterator;
 
-    static nodes_iterator nodes_begin(GraphPtr* graph)
-    {
-      return (*graph)->verticesBegin();
-    }
+  static nodes_iterator nodes_begin(GraphPtr *graph) {
+    return (*graph)->verticesBegin();
+  }
 
-    static nodes_iterator nodes_end(GraphPtr* graph)
-    {
-      return (*graph)->verticesEnd();
-    }
+  static nodes_iterator nodes_end(GraphPtr *graph) {
+    return (*graph)->verticesEnd();
+  }
 
-    using EdgeRef = typename Graph::EdgeDescriptor;
-    using ChildEdgeIteratorType = typename Graph::IncidentEdgeIterator;
+  using EdgeRef = typename Graph::EdgeDescriptor;
+  using ChildEdgeIteratorType = typename Graph::IncidentEdgeIterator;
 
-    static ChildEdgeIteratorType child_edge_begin(NodeRef node)
-    {
-      return node.graph->outgoingEdgesBegin(node);
-    }
+  static ChildEdgeIteratorType child_edge_begin(NodeRef node) {
+    return node.graph->outgoingEdgesBegin(node);
+  }
 
-    static ChildEdgeIteratorType child_edge_end(NodeRef node)
-    {
-      return node.graph->outgoingEdgesEnd(node);
-    }
+  static ChildEdgeIteratorType child_edge_end(NodeRef node) {
+    return node.graph->outgoingEdgesEnd(node);
+  }
 
-    static NodeRef edge_dest(EdgeRef edge)
-    {
-      return edge.to;
-    }
+  static NodeRef edge_dest(EdgeRef edge) { return edge.to; }
 
-    static size_t size(GraphPtr* graph)
-    {
-      return (*graph)->size();
-    }
-  };
-}
+  static size_t size(GraphPtr *graph) { return (*graph)->size(); }
+};
+} // namespace llvm
 
 #endif // MARCO_MODELING_SINGLEENTRYCONNECTEDDIGRAPH_H
