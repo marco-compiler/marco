2702e70b6c908dc9281c7dfb1083522a623deaa8
Michele Scuttari <michele.scuttari@outlook.com>
2024-12-07 21:59:38 +0100

    Fix discarded result types during bufferization of runtime's FunctionOp

--- lib/Dialect/Runtime/IR/Ops.cpp
+++ lib/Dialect/Runtime/IR/Ops.cpp
@@ -1,14 +1,13 @@
 #include "marco/Dialect/Runtime/IR/Ops.h"
 #include "marco/Dialect/Runtime/IR/Runtime.h"
-#include "mlir/Interfaces/FunctionImplementation.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/OpImplementation.h"
+#include "mlir/Interfaces/FunctionImplementation.h"
 
 using namespace ::mlir::runtime;
 
-static bool parsePrintableIndicesList(
-    mlir::OpAsmParser& parser, PrintableIndicesList& prop)
-{
+static bool parsePrintableIndicesList(mlir::OpAsmParser &parser,
+                                      PrintableIndicesList &prop) {
   if (parser.parseLSquare()) {
     return true;
   }
@@ -38,14 +37,12 @@ static bool parsePrintableIndicesList(
   return false;
 }
 
-static void printPrintableIndicesList(
-    mlir::OpAsmPrinter& printer,
-    mlir::Operation* op,
-    const PrintableIndicesList& prop)
-{
+static void printPrintableIndicesList(mlir::OpAsmPrinter &printer,
+                                      mlir::Operation *op,
+                                      const PrintableIndicesList &prop) {
   printer << "[";
 
-  llvm::interleaveComma(prop, printer, [&](const PrintInfo& printInfo) {
+  llvm::interleaveComma(prop, printer, [&](const PrintInfo &printInfo) {
     if (printInfo.isa<bool>()) {
       printer << (printInfo.get<bool>() ? "true" : "false");
       return;
@@ -63,282 +60,264 @@ static void printPrintableIndicesList(
 //===---------------------------------------------------------------------===//
 // InitFunctionOp
 
-namespace mlir::runtime
-{
-  mlir::ParseResult InitFunctionOp::parse(
-      mlir::OpAsmParser& parser, mlir::OperationState& result)
-  {
-    auto& builder = parser.getBuilder();
-    mlir::Region* bodyRegion = result.addRegion();
+namespace mlir::runtime {
+mlir::ParseResult InitFunctionOp::parse(mlir::OpAsmParser &parser,
+                                        mlir::OperationState &result) {
+  auto &builder = parser.getBuilder();
+  mlir::Region *bodyRegion = result.addRegion();
 
-    if (parser.parseOptionalAttrDictWithKeyword(result.attributes) ||
-        parser.parseRegion(*bodyRegion)) {
-      return mlir::failure();
-    }
-
-    result.addAttribute(
-        getFunctionTypeAttrName(result.name),
-        mlir::TypeAttr::get(builder.getFunctionType(
-            std::nullopt, std::nullopt)));
-
-    if (bodyRegion->empty()) {
-      bodyRegion->emplaceBlock();
-    }
-
-    return mlir::success();
+  if (parser.parseOptionalAttrDictWithKeyword(result.attributes) ||
+      parser.parseRegion(*bodyRegion)) {
+    return mlir::failure();
   }
 
-  void InitFunctionOp::print(mlir::OpAsmPrinter& printer)
-  {
-    llvm::SmallVector<llvm::StringRef, 1> elidedAttrs;
-    elidedAttrs.push_back(getFunctionTypeAttrName().getValue());
+  result.addAttribute(
+      getFunctionTypeAttrName(result.name),
+      mlir::TypeAttr::get(builder.getFunctionType(std::nullopt, std::nullopt)));
 
-    printer.printOptionalAttrDictWithKeyword(
-        getOperation()->getAttrs(), elidedAttrs);
-
-    printer << " ";
-    printer.printRegion(getBodyRegion(), false);
+  if (bodyRegion->empty()) {
+    bodyRegion->emplaceBlock();
   }
+
+  return mlir::success();
 }
+
+void InitFunctionOp::print(mlir::OpAsmPrinter &printer) {
+  llvm::SmallVector<llvm::StringRef, 1> elidedAttrs;
+  elidedAttrs.push_back(getFunctionTypeAttrName().getValue());
+
+  printer.printOptionalAttrDictWithKeyword(getOperation()->getAttrs(),
+                                           elidedAttrs);
+
+  printer << " ";
+  printer.printRegion(getBodyRegion(), false);
+}
+} // namespace mlir::runtime
 
 //===---------------------------------------------------------------------===//
 // DeinitOp
 
-namespace mlir::runtime
-{
-  mlir::ParseResult DeinitFunctionOp::parse(
-      mlir::OpAsmParser& parser, mlir::OperationState& result)
-  {
-    auto& builder = parser.getBuilder();
-    mlir::Region* bodyRegion = result.addRegion();
+namespace mlir::runtime {
+mlir::ParseResult DeinitFunctionOp::parse(mlir::OpAsmParser &parser,
+                                          mlir::OperationState &result) {
+  auto &builder = parser.getBuilder();
+  mlir::Region *bodyRegion = result.addRegion();
 
-    if (parser.parseOptionalAttrDictWithKeyword(result.attributes) ||
-        parser.parseRegion(*bodyRegion)) {
-      return mlir::failure();
-    }
-
-    result.addAttribute(
-        getFunctionTypeAttrName(result.name),
-        mlir::TypeAttr::get(builder.getFunctionType(
-            std::nullopt, std::nullopt)));
-
-    if (bodyRegion->empty()) {
-      bodyRegion->emplaceBlock();
-    }
-
-    return mlir::success();
+  if (parser.parseOptionalAttrDictWithKeyword(result.attributes) ||
+      parser.parseRegion(*bodyRegion)) {
+    return mlir::failure();
   }
 
-  void DeinitFunctionOp::print(mlir::OpAsmPrinter& printer)
-  {
-    llvm::SmallVector<llvm::StringRef, 1> elidedAttrs;
-    elidedAttrs.push_back(getFunctionTypeAttrName().getValue());
+  result.addAttribute(
+      getFunctionTypeAttrName(result.name),
+      mlir::TypeAttr::get(builder.getFunctionType(std::nullopt, std::nullopt)));
 
-    printer.printOptionalAttrDictWithKeyword(
-        getOperation()->getAttrs(), elidedAttrs);
-
-    printer << " ";
-    printer.printRegion(getBodyRegion(), false);
+  if (bodyRegion->empty()) {
+    bodyRegion->emplaceBlock();
   }
+
+  return mlir::success();
 }
+
+void DeinitFunctionOp::print(mlir::OpAsmPrinter &printer) {
+  llvm::SmallVector<llvm::StringRef, 1> elidedAttrs;
+  elidedAttrs.push_back(getFunctionTypeAttrName().getValue());
+
+  printer.printOptionalAttrDictWithKeyword(getOperation()->getAttrs(),
+                                           elidedAttrs);
+
+  printer << " ";
+  printer.printRegion(getBodyRegion(), false);
+}
+} // namespace mlir::runtime
 
 //===---------------------------------------------------------------------===//
 // VariableGetterOp
 
-namespace mlir::runtime
-{
-  void VariableGetterOp::build(
-      mlir::OpBuilder& builder,
-      mlir::OperationState& state,
-      llvm::StringRef name,
-      uint64_t variableRank)
-  {
-    state.addAttribute(
-        mlir::SymbolTable::getSymbolAttrName(),
-        builder.getStringAttr(name));
+namespace mlir::runtime {
+void VariableGetterOp::build(mlir::OpBuilder &builder,
+                             mlir::OperationState &state, llvm::StringRef name,
+                             uint64_t variableRank) {
+  state.addAttribute(mlir::SymbolTable::getSymbolAttrName(),
+                     builder.getStringAttr(name));
 
-    llvm::SmallVector<mlir::Type, 3> argTypes(
-        variableRank, builder.getIndexType());
+  llvm::SmallVector<mlir::Type, 3> argTypes(variableRank,
+                                            builder.getIndexType());
 
-    state.addAttribute(
-        getFunctionTypeAttrName(state.name),
-        mlir::TypeAttr::get(builder.getFunctionType(
-            argTypes, builder.getF64Type())));
+  state.addAttribute(getFunctionTypeAttrName(state.name),
+                     mlir::TypeAttr::get(builder.getFunctionType(
+                         argTypes, builder.getF64Type())));
 
-    state.addRegion();
-  }
-
-  mlir::ParseResult VariableGetterOp::parse(
-      mlir::OpAsmParser& parser, mlir::OperationState& result)
-  {
-    auto buildFuncType =
-        [](mlir::Builder& builder,
-           llvm::ArrayRef<mlir::Type> argTypes,
-           llvm::ArrayRef<mlir::Type> results,
-           mlir::function_interface_impl::VariadicFlag,
-           std::string&) {
-          return builder.getFunctionType(argTypes, results);
-        };
-
-    return mlir::function_interface_impl::parseFunctionOp(
-        parser, result, false,
-        getFunctionTypeAttrName(result.name), buildFuncType,
-        getArgAttrsAttrName(result.name), getResAttrsAttrName(result.name));
-  }
-
-  void VariableGetterOp::print(mlir::OpAsmPrinter& printer)
-  {
-    mlir::function_interface_impl::printFunctionOp(
-        printer, *this, false, getFunctionTypeAttrName(),
-        getArgAttrsAttrName(), getResAttrsAttrName());
-  }
-
-  mlir::BlockArgument VariableGetterOp::getVariable()
-  {
-    return getBodyRegion().getArgument(0);
-  }
-
-  uint64_t VariableGetterOp::getVariableRank()
-  {
-    return getIndices().size();
-  }
-
-  llvm::ArrayRef<mlir::BlockArgument> VariableGetterOp::getIndices()
-  {
-    return getBodyRegion().getArguments();
-  }
-
-  mlir::BlockArgument VariableGetterOp::getIndex(uint64_t dimension)
-  {
-    return getBodyRegion().getArgument(dimension);
-  }
+  state.addRegion();
 }
+
+mlir::ParseResult VariableGetterOp::parse(mlir::OpAsmParser &parser,
+                                          mlir::OperationState &result) {
+  auto buildFuncType =
+      [](mlir::Builder &builder, llvm::ArrayRef<mlir::Type> argTypes,
+         llvm::ArrayRef<mlir::Type> results,
+         mlir::function_interface_impl::VariadicFlag,
+         std::string &) { return builder.getFunctionType(argTypes, results); };
+
+  return mlir::function_interface_impl::parseFunctionOp(
+      parser, result, false, getFunctionTypeAttrName(result.name),
+      buildFuncType, getArgAttrsAttrName(result.name),
+      getResAttrsAttrName(result.name));
+}
+
+void VariableGetterOp::print(mlir::OpAsmPrinter &printer) {
+  mlir::function_interface_impl::printFunctionOp(
+      printer, *this, false, getFunctionTypeAttrName(), getArgAttrsAttrName(),
+      getResAttrsAttrName());
+}
+
+mlir::BlockArgument VariableGetterOp::getVariable() {
+  return getBodyRegion().getArgument(0);
+}
+
+uint64_t VariableGetterOp::getVariableRank() { return getIndices().size(); }
+
+llvm::ArrayRef<mlir::BlockArgument> VariableGetterOp::getIndices() {
+  return getBodyRegion().getArguments();
+}
+
+mlir::BlockArgument VariableGetterOp::getIndex(uint64_t dimension) {
+  return getBodyRegion().getArgument(dimension);
+}
+} // namespace mlir::runtime
 
 //===---------------------------------------------------------------------===//
 // EquationFunctionOp
 
-namespace mlir::runtime
-{
-  mlir::ParseResult EquationFunctionOp::parse(
-      mlir::OpAsmParser& parser, mlir::OperationState& result)
-  {
-    auto buildFuncType =
-        [](mlir::Builder& builder,
-           llvm::ArrayRef<mlir::Type> argTypes,
-           llvm::ArrayRef<mlir::Type> results,
-           mlir::function_interface_impl::VariadicFlag,
-           std::string&) {
-          return builder.getFunctionType(argTypes, results);
-        };
+namespace mlir::runtime {
+mlir::ParseResult EquationFunctionOp::parse(mlir::OpAsmParser &parser,
+                                            mlir::OperationState &result) {
+  auto buildFuncType =
+      [](mlir::Builder &builder, llvm::ArrayRef<mlir::Type> argTypes,
+         llvm::ArrayRef<mlir::Type> results,
+         mlir::function_interface_impl::VariadicFlag,
+         std::string &) { return builder.getFunctionType(argTypes, results); };
 
-    return mlir::function_interface_impl::parseFunctionOp(
-        parser, result, false,
-        getFunctionTypeAttrName(result.name), buildFuncType,
-        getArgAttrsAttrName(result.name), getResAttrsAttrName(result.name));
-  }
-
-  void EquationFunctionOp::print(OpAsmPrinter& printer)
-  {
-    mlir::function_interface_impl::printFunctionOp(
-        printer, *this, false, getFunctionTypeAttrName(),
-        getArgAttrsAttrName(), getResAttrsAttrName());
-  }
-
-  void EquationFunctionOp::build(
-      mlir::OpBuilder& builder,
-      mlir::OperationState& state,
-      llvm::StringRef name,
-      uint64_t numOfInductions,
-      llvm::ArrayRef<mlir::NamedAttribute> attrs,
-      llvm::ArrayRef<mlir::DictionaryAttr> argAttrs)
-  {
-    state.addAttribute(
-        mlir::SymbolTable::getSymbolAttrName(),
-        builder.getStringAttr(name));
-
-    llvm::SmallVector<mlir::Type> argTypes(
-        numOfInductions * 2, builder.getIndexType());
-
-    auto functionType = builder.getFunctionType(argTypes, std::nullopt);
-
-    state.addAttribute(
-        getFunctionTypeAttrName(state.name),
-        mlir::TypeAttr::get(functionType));
-
-    state.attributes.append(attrs.begin(), attrs.end());
-    state.addRegion();
-
-    if (argAttrs.empty()) {
-      return;
-    }
-
-    assert(functionType.getNumInputs() == argAttrs.size());
-
-    mlir::function_interface_impl::addArgAndResultAttrs(
-        builder, state, argAttrs, std::nullopt,
-        getArgAttrsAttrName(state.name), getResAttrsAttrName(state.name));
-  }
+  return mlir::function_interface_impl::parseFunctionOp(
+      parser, result, false, getFunctionTypeAttrName(result.name),
+      buildFuncType, getArgAttrsAttrName(result.name),
+      getResAttrsAttrName(result.name));
 }
+
+void EquationFunctionOp::print(OpAsmPrinter &printer) {
+  mlir::function_interface_impl::printFunctionOp(
+      printer, *this, false, getFunctionTypeAttrName(), getArgAttrsAttrName(),
+      getResAttrsAttrName());
+}
+
+void EquationFunctionOp::build(mlir::OpBuilder &builder,
+                               mlir::OperationState &state,
+                               llvm::StringRef name, uint64_t numOfInductions,
+                               llvm::ArrayRef<mlir::NamedAttribute> attrs,
+                               llvm::ArrayRef<mlir::DictionaryAttr> argAttrs) {
+  state.addAttribute(mlir::SymbolTable::getSymbolAttrName(),
+                     builder.getStringAttr(name));
+
+  llvm::SmallVector<mlir::Type> argTypes(numOfInductions * 2,
+                                         builder.getIndexType());
+
+  auto functionType = builder.getFunctionType(argTypes, std::nullopt);
+
+  state.addAttribute(getFunctionTypeAttrName(state.name),
+                     mlir::TypeAttr::get(functionType));
+
+  state.attributes.append(attrs.begin(), attrs.end());
+  state.addRegion();
+
+  if (argAttrs.empty()) {
+    return;
+  }
+
+  assert(functionType.getNumInputs() == argAttrs.size());
+
+  mlir::function_interface_impl::addArgAndResultAttrs(
+      builder, state, argAttrs, std::nullopt, getArgAttrsAttrName(state.name),
+      getResAttrsAttrName(state.name));
+}
+} // namespace mlir::runtime
 
 //===---------------------------------------------------------------------===//
 // FunctionOp
 
-namespace mlir::runtime
-{
-  void FunctionOp::build(
-      mlir::OpBuilder& builder,
-      mlir::OperationState& state,
-      llvm::StringRef name,
-      mlir::FunctionType functionType)
-  {
-    state.addAttribute(
-        mlir::SymbolTable::getSymbolAttrName(),
-        builder.getStringAttr(name));
+namespace mlir::runtime {
+void FunctionOp::build(mlir::OpBuilder &builder, mlir::OperationState &state,
+                       llvm::StringRef name, mlir::FunctionType functionType) {
+  state.addAttribute(mlir::SymbolTable::getSymbolAttrName(),
+                     builder.getStringAttr(name));
 
-    state.addAttribute(
-        getFunctionTypeAttrName(state.name),
-        mlir::TypeAttr::get(functionType));
+  state.addAttribute(getFunctionTypeAttrName(state.name),
+                     mlir::TypeAttr::get(functionType));
 
-    state.addRegion();
-  }
-
-  mlir::ParseResult FunctionOp::parse(
-      mlir::OpAsmParser& parser, mlir::OperationState& result)
-  {
-    auto buildFuncType =
-        [](mlir::Builder& builder,
-           llvm::ArrayRef<mlir::Type> argTypes,
-           llvm::ArrayRef<mlir::Type> results,
-           mlir::function_interface_impl::VariadicFlag,
-           std::string&) {
-          return builder.getFunctionType(argTypes, results);
-        };
-
-    return mlir::function_interface_impl::parseFunctionOp(
-        parser, result, false,
-        getFunctionTypeAttrName(result.name), buildFuncType,
-        getArgAttrsAttrName(result.name), getResAttrsAttrName(result.name));
-  }
-
-  void FunctionOp::print(mlir::OpAsmPrinter& printer)
-  {
-    mlir::function_interface_impl::printFunctionOp(
-        printer, *this, false, getFunctionTypeAttrName(),
-        getArgAttrsAttrName(), getResAttrsAttrName());
-  }
+  state.addRegion();
 }
+
+mlir::ParseResult FunctionOp::parse(mlir::OpAsmParser &parser,
+                                    mlir::OperationState &result) {
+  auto buildFuncType =
+      [](mlir::Builder &builder, llvm::ArrayRef<mlir::Type> argTypes,
+         llvm::ArrayRef<mlir::Type> results,
+         mlir::function_interface_impl::VariadicFlag,
+         std::string &) { return builder.getFunctionType(argTypes, results); };
+
+  return mlir::function_interface_impl::parseFunctionOp(
+      parser, result, false, getFunctionTypeAttrName(result.name),
+      buildFuncType, getArgAttrsAttrName(result.name),
+      getResAttrsAttrName(result.name));
+}
+
+void FunctionOp::print(mlir::OpAsmPrinter &printer) {
+  mlir::function_interface_impl::printFunctionOp(
+      printer, *this, false, getFunctionTypeAttrName(), getArgAttrsAttrName(),
+      getResAttrsAttrName());
+}
+} // namespace mlir::runtime
 
 //===---------------------------------------------------------------------===//
 // CallOp
 
-namespace mlir::runtime
-{
-  void CallOp::build(
-      mlir::OpBuilder& builder,
-      mlir::OperationState& state,
-      FunctionOp callee,
-      mlir::ValueRange args)
-  {
-    build(builder, state, callee.getResultTypes(), callee.getSymName(), args);
-  }
+namespace mlir::runtime {
+void CallOp::build(mlir::OpBuilder &builder, mlir::OperationState &state,
+                   FunctionOp callee, mlir::ValueRange args) {
+  build(builder, state, callee.getResultTypes(), callee.getSymName(), args);
 }
+
+mlir::LogicalResult CallOp::verify() {
+  if (llvm::any_of(getResultTypes(), [](mlir::Type type) {
+        return mlir::isa<mlir::ShapedType>(type);
+      })) {
+    return emitOpError() << "results must be scalar values";
+  }
+
+  return mlir::success();
+}
+
+mlir::LogicalResult
+CallOp::verifySymbolUses(mlir::SymbolTableCollection &symbolTable) {
+  auto moduleOp = getOperation()->getParentOfType<mlir::ModuleOp>();
+
+  auto functionOp =
+      symbolTable.lookupSymbolIn<FunctionOp>(moduleOp, getCalleeAttr());
+
+  if (!functionOp) {
+    return emitOpError() << "'" << getCallee()
+                         << "' does not reference a valid function";
+  }
+
+  mlir::FunctionType functionType = functionOp.getFunctionType();
+
+  if (getArgs().getTypes() != functionType.getInputs()) {
+    return emitOpError() << "arguments don't match the function signature";
+  }
+
+  if (getResultTypes() != functionType.getResults()) {
+    return emitOpError() << "results don't match the function signature";
+  }
+
+  return mlir::success();
+}
+} // namespace mlir::runtime
--- lib/Dialect/Runtime/Transforms/BufferizableOpInterfaceImpl.cpp
+++ lib/Dialect/Runtime/Transforms/BufferizableOpInterfaceImpl.cpp
@@ -5,148 +5,131 @@
 using namespace ::mlir::runtime;
 using namespace ::mlir::bufferization;
 
-namespace
-{
-  struct FunctionOpInterface
-      : public BufferizableOpInterface::ExternalModel<
-            FunctionOpInterface, FunctionOp>
-  {
-    mlir::LogicalResult bufferize(
-        mlir::Operation* op,
-        mlir::RewriterBase& rewriter,
-        const BufferizationOptions& options) const
-    {
-      auto functionOp = mlir::cast<FunctionOp>(op);
+namespace {
+struct FunctionOpInterface
+    : public BufferizableOpInterface::ExternalModel<FunctionOpInterface,
+                                                    FunctionOp> {
+  mlir::LogicalResult bufferize(mlir::Operation *op,
+                                mlir::RewriterBase &rewriter,
+                                const BufferizationOptions &options) const {
+    auto functionOp = mlir::cast<FunctionOp>(op);
 
-      llvm::SmallVector<mlir::Type> args;
-      llvm::SmallVector<mlir::Type> results;
+    llvm::SmallVector<mlir::Type> args;
+    llvm::SmallVector<mlir::Type> results;
 
-      for (mlir::Type arg : functionOp.getArgumentTypes()) {
-        if (auto tensorType = arg.dyn_cast<mlir::RankedTensorType>()) {
-          args.push_back(mlir::MemRefType::get(
-              tensorType.getShape(), tensorType.getElementType()));
+    assert(llvm::all_of(functionOp.getResultTypes(), [](mlir::Type type) {
+      return !mlir::isa<mlir::ShapedType>(type);
+    }));
 
-          continue;
+    llvm::append_range(results, functionOp.getResultTypes());
+
+    for (mlir::Type arg : functionOp.getArgumentTypes()) {
+      if (auto tensorType = arg.dyn_cast<mlir::RankedTensorType>()) {
+        args.push_back(mlir::MemRefType::get(tensorType.getShape(),
+                                             tensorType.getElementType()));
+
+        continue;
+      }
+
+      if (auto tensorType = arg.dyn_cast<mlir::UnrankedTensorType>()) {
+        auto memSpace = options.defaultMemorySpaceFn(tensorType);
+
+        if (!memSpace) {
+          return mlir::failure();
         }
 
-        if (auto tensorType = arg.dyn_cast<mlir::UnrankedTensorType>()) {
-          auto memSpace = options.defaultMemorySpaceFn(tensorType);
+        args.push_back(mlir::UnrankedMemRefType::get(
+            tensorType.getElementType(), *memSpace));
 
-          if (!memSpace) {
-            return mlir::failure();
-          }
+        continue;
+      }
 
-          args.push_back(mlir::UnrankedMemRefType::get(
-              tensorType.getElementType(), *memSpace));
+      args.push_back(arg);
+    }
 
-          continue;
+    auto visibility = functionOp.getVisibility();
+
+    auto replacement = replaceOpWithNewBufferizedOp<FunctionOp>(
+        rewriter, op, functionOp.getSymName(),
+        rewriter.getFunctionType(args, results));
+
+    replacement.setVisibility(visibility);
+    return mlir::success();
+  }
+
+  bool hasTensorSemantics(mlir::Operation *op) const {
+    auto isaTensor = [](mlir::Type type) {
+      return mlir::isa<mlir::RankedTensorType, mlir::UnrankedTensorType>(type);
+    };
+
+    auto functionOp = mlir::cast<FunctionOp>(op);
+
+    bool hasTensorArg = llvm::any_of(functionOp.getArgumentTypes(), isaTensor);
+
+    bool hasTensorResult = llvm::any_of(functionOp.getResultTypes(), isaTensor);
+
+    if (hasTensorArg || hasTensorResult) {
+      return true;
+    }
+
+    return false;
+  }
+};
+
+struct CallOpInterface
+    : public BufferizableOpInterface::ExternalModel<CallOpInterface, CallOp> {
+  bool bufferizesToMemoryRead(mlir::Operation *op, mlir::OpOperand &opOperand,
+                              const AnalysisState &state) const {
+    return false;
+  }
+
+  bool bufferizesToMemoryWrite(mlir::Operation *op, mlir::OpOperand &opOperand,
+                               const AnalysisState &state) const {
+    return false;
+  }
+
+  mlir::LogicalResult bufferize(mlir::Operation *op,
+                                mlir::RewriterBase &rewriter,
+                                const BufferizationOptions &options) const {
+    auto callOp = mlir::cast<CallOp>(op);
+    llvm::SmallVector<mlir::Value> args;
+
+    for (mlir::Value arg : callOp.getArgs()) {
+      if (arg.getType().isa<mlir::TensorType>()) {
+        auto argBuffer = getBuffer(rewriter, arg, options);
+
+        if (mlir::failed(argBuffer)) {
+          return mlir::failure();
         }
 
+        args.push_back(*argBuffer);
+      } else {
         args.push_back(arg);
       }
-
-      auto visibility = functionOp.getVisibility();
-
-      auto replacement = replaceOpWithNewBufferizedOp<FunctionOp>(
-          rewriter, op, functionOp.getSymName(),
-          rewriter.getFunctionType(args, results));
-
-      replacement.setVisibility(visibility);
-      return mlir::success();
     }
 
-    bool hasTensorSemantics(mlir::Operation* op) const
-    {
-      auto isaTensor = [](mlir::Type type) {
-        return mlir::isa<
-            mlir::RankedTensorType,
-            mlir::UnrankedTensorType>(type);
-      };
+    replaceOpWithNewBufferizedOp<CallOp>(rewriter, op, callOp.getResultTypes(),
+                                         callOp.getCallee(), args);
 
-      auto functionOp = mlir::cast<FunctionOp>(op);
-
-      bool hasTensorArg =
-          llvm::any_of(functionOp.getArgumentTypes(), isaTensor);
-
-      bool hasTensorResult =
-          llvm::any_of(functionOp.getResultTypes(), isaTensor);
-
-      if (hasTensorArg || hasTensorResult) {
-        return true;
-      }
-
-      return false;
-    }
-  };
-
-  struct CallOpInterface
-      : public BufferizableOpInterface::ExternalModel<
-            CallOpInterface, CallOp>
-  {
-    bool bufferizesToMemoryRead(
-        mlir::Operation* op,
-        mlir::OpOperand& opOperand,
-        const AnalysisState& state) const
-    {
-      return false;
-    }
-
-    bool bufferizesToMemoryWrite(
-        mlir::Operation* op,
-        mlir::OpOperand& opOperand,
-        const AnalysisState& state) const
-    {
-      return false;
-    }
-
-    mlir::LogicalResult bufferize(
-        mlir::Operation* op,
-        mlir::RewriterBase& rewriter,
-        const BufferizationOptions& options) const
-    {
-      auto callOp = mlir::cast<CallOp>(op);
-      llvm::SmallVector<mlir::Value> args;
-
-      for (mlir::Value arg : callOp.getArgs()) {
-        if (arg.getType().isa<mlir::TensorType>()) {
-          auto argBuffer = getBuffer(rewriter, arg, options);
-
-          if (mlir::failed(argBuffer)) {
-            return mlir::failure();
-          }
-
-          args.push_back(*argBuffer);
-        } else {
-          args.push_back(arg);
-        }
-      }
-
-      replaceOpWithNewBufferizedOp<CallOp>(
-          rewriter, op, callOp.getResultTypes(), callOp.getCallee(), args);
-
-      return mlir::success();
-    }
-
-    AliasingValueList getAliasingValues(
-        mlir::Operation* op,
-        mlir::OpOperand& opOperand,
-        const AnalysisState& state) const
-    {
-      return {{op->getOperand(opOperand.getOperandNumber()),
-               BufferRelation::Equivalent}};
-    }
-  };
-}
-
-namespace mlir::runtime
-{
-  void registerBufferizableOpInterfaceExternalModels(
-      mlir::DialectRegistry& registry)
-  {
-    registry.addExtension(+[](mlir::MLIRContext* context,
-                              RuntimeDialect* dialect) {
-      FunctionOp::attachInterface<::FunctionOpInterface>(*context);
-      CallOp::attachInterface<::CallOpInterface>(*context);
-    });
+    return mlir::success();
   }
+
+  AliasingValueList getAliasingValues(mlir::Operation *op,
+                                      mlir::OpOperand &opOperand,
+                                      const AnalysisState &state) const {
+    return {{op->getOperand(opOperand.getOperandNumber()),
+             BufferRelation::Equivalent}};
+  }
+};
+} // namespace
+
+namespace mlir::runtime {
+void registerBufferizableOpInterfaceExternalModels(
+    mlir::DialectRegistry &registry) {
+  registry.addExtension(
+      +[](mlir::MLIRContext *context, RuntimeDialect *dialect) {
+        FunctionOp::attachInterface<::FunctionOpInterface>(*context);
+        CallOp::attachInterface<::CallOpInterface>(*context);
+      });
 }
+} // namespace mlir::runtime
