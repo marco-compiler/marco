ee66dddd14a9d22e0e9fac71a2c5a818a5865899
Michele Scuttari <michele.scuttari@outlook.com>
2024-12-08 23:51:27 +0100

    Register all extensions and translations in optimizer driver

--- tool/modelica-opt/modelica-opt.cpp
+++ tool/modelica-opt/modelica-opt.cpp
@@ -1,35 +1,36 @@
-#include "marco/Dialect/IDA/IR/IDA.h"
-#include "marco/Dialect/KINSOL/IR/KINSOL.h"
+#include "marco/Codegen/Conversion/Passes.h"
+#include "marco/Codegen/Lowering/Bridge.h"
 #include "marco/Dialect/BaseModelica/IR/BaseModelica.h"
 #include "marco/Dialect/BaseModelica/Transforms/AllInterfaces.h"
 #include "marco/Dialect/BaseModelica/Transforms/Passes.h"
+#include "marco/Dialect/IDA/IR/IDA.h"
+#include "marco/Dialect/KINSOL/IR/KINSOL.h"
 #include "marco/Dialect/Runtime/IR/Runtime.h"
 #include "marco/Dialect/Runtime/Transforms/AllInterfaces.h"
 #include "marco/Dialect/Runtime/Transforms/Passes.h"
 #include "marco/Dialect/SUNDIALS/IR/SUNDIALS.h"
-#include "marco/Codegen/Lowering/Bridge.h"
-#include "marco/Codegen/Conversion/Passes.h"
 #include "mlir/InitAllDialects.h"
+#include "mlir/InitAllExtensions.h"
 #include "mlir/InitAllPasses.h"
-#include "mlir/Dialect/Func/Extensions/AllExtensions.h"
+#include "mlir/Target/LLVMIR/Dialect/Builtin/BuiltinToLLVMIRTranslation.h"
+#include "mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
   // Register the dialects.
-	mlir::DialectRegistry registry;
+  mlir::DialectRegistry registry;
 
   mlir::registerAllDialects(registry);
 
   registry.insert<mlir::ida::IDADialect>();
   registry.insert<mlir::kinsol::KINSOLDialect>();
-	registry.insert<mlir::bmodelica::BaseModelicaDialect>();
+  registry.insert<mlir::bmodelica::BaseModelicaDialect>();
   registry.insert<mlir::modeling::ModelingDialect>();
   registry.insert<mlir::runtime::RuntimeDialect>();
   registry.insert<mlir::sundials::SUNDIALSDialect>();
 
   // Register the extensions.
-  mlir::func::registerAllExtensions(registry);
+  mlir::registerAllExtensions(registry);
 
   // Register the external models.
   mlir::bmodelica::registerAllDialectInterfaceImplementations(registry);
@@ -44,8 +45,12 @@ int main(int argc, char* argv[])
   // Register MLIR built-in transformations.
   mlir::registerAllPasses();
 
-	auto result = mlir::MlirOptMain(
-      argc, argv, "Modelica optimizer driver\n", registry);
+  // Register the dialect translations.
+  mlir::registerBuiltinDialectTranslation(registry);
+  mlir::registerLLVMDialectTranslation(registry);
 
-	return mlir::asMainReturnCode(result);
+  auto result =
+      mlir::MlirOptMain(argc, argv, "Modelica optimizer driver\n", registry);
+
+  return mlir::asMainReturnCode(result);
 }
