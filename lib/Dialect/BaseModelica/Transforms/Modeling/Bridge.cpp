4220762acb0e0e5dccc682935c70dfa6de4e0b24
Michele Scuttari <michele.scuttari@outlook.com>
2025-06-27 21:30:00 +0200

    Move methods to different compilation unit

--- lib/Dialect/BaseModelica/Transforms/Modeling/Bridge.cpp
+++ lib/Dialect/BaseModelica/Transforms/Modeling/Bridge.cpp
@@ -8,4 +8,22 @@ struct StorageImpl : Storage {};
 std::unique_ptr<Storage> Storage::create() {
   return std::make_unique<StorageImpl>();
 }
+
+void Storage::clear() {
+  clearVariables();
+  clearEquations();
+  clearSCCs();
+}
+
+void Storage::clearVariables() {
+  variableBridges.clear();
+  variablesMap.clear();
+}
+
+void Storage::clearEquations() {
+  equationBridges.clear();
+  equationsMap.clear();
+}
+
+void Storage::clearSCCs() { sccBridges.clear(); }
 } // namespace mlir::bmodelica::bridge
