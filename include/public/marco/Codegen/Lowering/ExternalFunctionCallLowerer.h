f76b59b5d63b8095967f9b2db77dbefc1aa3138f
ImAndreaBellani <andrea1.bellani@mail.polimi.it>
2025-07-04 00:09:52 +0200

    added setFatherName (adjustments)

--- include/public/marco/Codegen/Lowering/ExternalFunctionCallLowerer.h
+++ include/public/marco/Codegen/Lowering/ExternalFunctionCallLowerer.h
@@ -17,6 +17,43 @@ namespace marco::codegen::lowering {
   protected:
     using Lowerer::lower;
 
+  private:
+    std::optional<mlir::Operation *>
+  resolveCallee(const ast::ComponentReference &callee);
+
+  std::optional<mlir::Value> lowerArg(const ast::Expression &expression);
+
+  void getCustomFunctionInputVariables(
+      llvm::SmallVectorImpl<mlir::bmodelica::VariableOp> &inputVariables,
+      mlir::bmodelica::FunctionOp functionOp);
+
+  [[nodiscard]] bool lowerCustomFunctionArgs(
+      const ast::Call &call,
+      llvm::ArrayRef<mlir::bmodelica::VariableOp> calleeInputs,
+      llvm::SmallVectorImpl<std::string> &argNames,
+      llvm::SmallVectorImpl<mlir::Value> &argValues);
+
+
+  /// Get the argument expected ranks of a user-defined function.
+  void getFunctionExpectedArgRanks(mlir::Operation *op,
+                                   llvm::SmallVectorImpl<int64_t> &ranks);
+
+  /// Get the result types of a user-defined function.
+  void getFunctionResultTypes(mlir::Operation *op,
+                              llvm::SmallVectorImpl<mlir::Type> &types);
+
+  /// Get the result type in case of a possibly element-wise call.
+  /// The arguments are needed because some functions (such as min / size)
+  /// may vary their behaviour according to arguments count.
+  
+
+  /// Helper function to emit an error if a function is provided the wrong
+  /// number of arguments. The error will state that the function received
+  /// actualNum arguments, but the expected number was exactly expectedNum.
+  void emitErrorNumArguments(llvm::StringRef function,
+                             const marco::SourceRange &location,
+                             unsigned int actualNum, unsigned int expectedNum);
+
   };
 }
 
--- lib/Codegen/Lowering/ExternalFunctionCallLowerer.cpp
+++ lib/Codegen/Lowering/ExternalFunctionCallLowerer.cpp
@@ -21,12 +21,125 @@ using namespace ::mlir::bmodelica;
 namespace marco::codegen::lowering {
 
 ExternalFunctionCallLowerer::ExternalFunctionCallLowerer(BridgeInterface *bridge) : Lowerer(bridge) {}
+  
+  void ExternalFunctionCallLowerer::getFunctionResultTypes(
+    mlir::Operation *op, llvm::SmallVectorImpl<mlir::Type> &types) {
+    assert((mlir::isa<FunctionOp>(op)));
+
+    if (auto functionOp = mlir::dyn_cast<FunctionOp>(op)) {
+      mlir::FunctionType functionType = functionOp.getFunctionType();
+      auto resultTypes = functionType.getResults();
+      types.append(resultTypes.begin(), resultTypes.end());
+      return;
+    }
+  }
+
+  void ExternalFunctionCallLowerer::getFunctionExpectedArgRanks(
+      mlir::Operation *op, llvm::SmallVectorImpl<int64_t> &ranks) {
+    assert((mlir::isa<FunctionOp, DerFunctionOp>(op)));
+
+    if (auto functionOp = mlir::dyn_cast<FunctionOp>(op)) {
+      mlir::FunctionType functionType = functionOp.getFunctionType();
+
+      for (mlir::Type type : functionType.getInputs()) {
+        if (auto shapedType = mlir::dyn_cast<mlir::ShapedType>(type)) {
+          ranks.push_back(shapedType.getRank());
+        } else {
+          ranks.push_back(0);
+        }
+      }
+
+      return;
+    }
+  }
+  bool ExternalFunctionCallLowerer::lowerCustomFunctionArgs(
+      const ast::Call &call, llvm::ArrayRef<VariableOp> calleeInputs,
+      llvm::SmallVectorImpl<std::string> &argNames,
+      llvm::SmallVectorImpl<mlir::Value> &argValues) {
+    size_t numOfArgs = call.getNumOfArguments();
+
+    if (numOfArgs != 0) {
+      if (call.getArgument(0)->dyn_cast<ast::ReductionFunctionArgument>()) {
+        assert(call.getNumOfArguments() == 1);
+        llvm_unreachable("ReductionOp has not been implemented in external function calls yet");
+        return false;
+      }
+    }
+
+    bool existsNamedArgument = false;
+
+    for (size_t i = 0; i < numOfArgs && !existsNamedArgument; ++i) {
+      if (call.getArgument(i)->isa<ast::NamedFunctionArgument>()) {
+        existsNamedArgument = true;
+      }
+    }
+
+    size_t argIndex = 0;
+
+    // Process the unnamed arguments.
+    while (argIndex < numOfArgs &&
+           !call.getArgument(argIndex)->isa<ast::NamedFunctionArgument>()) {
+      auto arg =
+          call.getArgument(argIndex)->cast<ast::ExpressionFunctionArgument>();
+
+      auto argValue = lowerArg(*arg->getExpression());
+      if (!argValue) {
+        return false;
+      }
+      argValues.push_back(*argValue);
+
+      if (existsNamedArgument) {
+        VariableOp variableOp = calleeInputs[argIndex];
+        argNames.push_back(variableOp.getSymName().str());
+      }
+
+      ++argIndex;
+    }
+
+    // Process the named arguments.
+    while (argIndex < numOfArgs) {
+      auto arg = call.getArgument(argIndex)->cast<ast::NamedFunctionArgument>();
+
+      auto argValue = lowerArg(*arg->getValue()
+                                    ->cast<ast::ExpressionFunctionArgument>()
+                                    ->getExpression());
+      if (!argValue) {
+        return false;
+      }
+      argValues.push_back(*argValue);
+
+      argNames.push_back(arg->getName().str());
+      ++argIndex;
+    }
+
+    return true;
+  }
+  void ExternalFunctionCallLowerer::getCustomFunctionInputVariables(
+      llvm::SmallVectorImpl<mlir::bmodelica::VariableOp> &inputVariables,
+      FunctionOp functionOp) {
+    for (VariableOp variableOp : functionOp.getVariables()) {
+      if (variableOp.isInput()) {
+        inputVariables.push_back(variableOp);
+      }
+    }
+  }
+  void ExternalFunctionCallLowerer::emitErrorNumArguments(llvm::StringRef function,
+                                          const marco::SourceRange &location,
+                                          unsigned int actualNum,
+                                          unsigned int expectedNum) {
+    std::string errorString =
+        function.str() + ": expected " + std::to_string(expectedNum) +
+        " argument(s) but got " + std::to_string(actualNum) + ".";
+    mlir::emitError(loc(location)) << errorString;
+  }
 
   std::optional<Results> ExternalFunctionCallLowerer::lower(const ast::ExternalFunctionCall &call) {
       
-  mlir::Operation *result = resolveSymbolName(call.getFatherName(), getLookupScope());
+      mlir::Operation *result = resolveSymbolName(call.getFatherName(), getLookupScope());
 
-    result = getSymbolTable().lookupSymbolIn(result, builder().getStringAttr(call->getFatherName()));
+      result = getSymbolTable().lookupSymbolIn(result, builder().getStringAttr(call.getFatherName()));
+
+      llvm::SmallVector<VariableOp> inputVariables;
 
       if (auto functionOp = mlir::dyn_cast<FunctionOp>(*result)) {
         getCustomFunctionInputVariables(inputVariables, functionOp);
@@ -48,7 +161,7 @@ ExternalFunctionCallLowerer::ExternalFunctionCallLowerer(BridgeInterface *bridge
       llvm::SmallVector<mlir::Type, 1> resultTypes;
 
       if (argValues.size() != expectedArgRanks.size()) {
-        emitErrorNumArguments(call->getName(),
+        emitErrorNumArguments(call.getName(),
                               callee->getElement(0)->getLocation(),
                               argValues.size(), expectedArgRanks.size());
         return std::nullopt;
