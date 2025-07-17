18c5aa4a4a55daa842b9aad90d873de7d76a1f78
andreaBruno_10686087_ <andrea2.bruno@mail.polimi.it>
2025-07-17 23:29:32 +0200

    fix

--- docker/marco.py
+++ docker/marco.py
@@ -7,7 +7,7 @@ import subprocess
 import sys
 
 DOCKER_SOCKET = "/var/run/docker.sock"
-DOCKER_IMAGE = "marco-local-46:latest"
+DOCKER_IMAGE = "marco-local-47:latest"
 INPUT_FILE_TYPES = (".mo", ".bmo", ".c", ".mlir", ".ll", ".o")
 
 def main():
