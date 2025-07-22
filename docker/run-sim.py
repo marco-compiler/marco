b5591e630cbc02aaa1327fa8c2df15dcfc16af63
andreaBruno_10686087_ <andrea2.bruno@mail.polimi.it>
2025-07-22 11:22:14 +0200

    public symbol

--- docker/run-sim.py
+++ docker/run-sim.py
@@ -7,7 +7,7 @@ import subprocess
 import sys
 
 DOCKER_SOCKET = "/var/run/docker.sock"
-DOCKER_IMAGE = "marco-local-49:latest"
+DOCKER_IMAGE = "marco-local-50:latest"
 
 def main():
     # Make sure the Docker client is installed.
