8a6d2892206df473777587cbdcb0f3430ca839d7
andreaBruno_10686087_ <andrea2.bruno@mail.polimi.it>
2025-07-22 13:58:29 +0200

    FunctionOpInterface

--- docker/marco.py
+++ docker/marco.py
@@ -7,7 +7,7 @@ import subprocess
 import sys
 
 DOCKER_SOCKET = "/var/run/docker.sock"
-DOCKER_IMAGE = "marco-local-52:latest"
+DOCKER_IMAGE = "marco-local-53:latest"
 INPUT_FILE_TYPES = (".mo", ".bmo", ".c", ".mlir", ".ll", ".o")
 
 def main():
